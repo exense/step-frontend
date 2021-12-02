f2d689ee3e651027eb926a5144fdb795ef2315d9
MalteRedschlag <malte.redschlag+github@gmail.com>
2021-12-02 16:25:44 +0100

    hybridization: angular app wrapping angularjs
    - Had to delete a typing file of the libs which had unresolved types.
    - angular.json currently serves all files of libs folder which might be enhanced in a future iteration
    - paths in index.html for loading the scripts are updated to find libraries
    
    Beware: index.html of "exense-visualization" dependency currently still points to bower_components which will fail.
    I will try to move this dependency to npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 @NgModule({
   declarations: [],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UpgradeModule,
   ],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) {}
+
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.body, ['tecAdminApp']);
+  }
+}
