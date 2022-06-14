c47811b25eead490d78e713b333485c3692eb7ea
Tim Rasim <neogucky@gmail.com>
2022-06-14 13:09:54 +0200

    SED-1088 migrating screenconfiguration table
    * Admin->Settings->Screens now using Mat Table
    
    * disabled sort, using TableLocalDataSource to search for attributes nested in dtos
    
    * fixed pipeline linting issues
    
    * fixed pipeline linting issues

---
+++ projects/step-frontend/src/lib/modules/admin/services/screen-dialogs.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ScreenInputDto, UibModalHelperService, a1Promise2Observable } from '@exense/step-core';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ScreenDialogsService {
+  constructor(private _httpClient: HttpClient, private _uibModalHelper: UibModalHelperService) {}
+
+  editScreen(
+    screen?: Partial<ScreenInputDto>,
+    screenDbId?: string,
+    screenChoice?: string
+  ): Observable<{ screen?: Partial<ScreenInputDto>; result: string }> {
+    const modalInstance = this._uibModalHelper.open({
+      backdrop: 'static',
+      templateUrl: 'partials/screenconfiguration/editScreenInputDialog.html',
+      controller: 'editScreenInputCtrl',
+      resolve: {
+        id: function () {
+          return screenDbId;
+        },
+        screenId: function () {
+          return screenChoice;
+        },
+      },
+    });
+
+    const result$ = a1Promise2Observable(modalInstance.result) as Observable<string>;
+    return result$.pipe(map((result) => ({ result, screen })));
+  }
+}
