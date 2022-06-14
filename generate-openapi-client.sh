9192989c4966b43718c75b67a74d481b40d7f8c4
Gilbert Fritz <gilbert.fritz@exense.ch>
2022-06-14 18:39:46 +0200

    fixes generation script and removes unused screens-api

--- generate-openapi-client.sh
+++ generate-openapi-client.sh
@@ -2,10 +2,10 @@
 
 SPEC_URL=$1
 GENERATED_CODE_LOCATION=./projects/step-core/src/lib/client/generated
-CLIENT_MODULE_NAME=StepClientModule.ts
+CLIENT_MODULE_NAME=StepClientModule
 
 npx openapi --input $SPEC_URL --output $GENERATED_CODE_LOCATION --client angular --useUnionTypes --name $CLIENT_MODULE_NAME --exportCore false --exportSchemas true
 
-# make services providedIn:'root' and remove them from the module's provided-array
+# make services global singletons and remove them from the module's provided-array
 sed -i "s/@Injectable()/@Injectable({providedIn:'root'})/g" $GENERATED_CODE_LOCATION/services/*Service.ts
-sed -i "/.*Service.*/d" $GENERATED_CODE_LOCATION/$CLIENT_MODULE_NAME
+sed -i "/.*Service.*/d" $GENERATED_CODE_LOCATION/$CLIENT_MODULE_NAME.ts
