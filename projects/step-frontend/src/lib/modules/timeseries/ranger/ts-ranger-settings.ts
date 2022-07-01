58fe8083cd0402929260e202783e1c3b0776099e
Lucian Baciu <103116491+lucianbaciu@users.noreply.github.com>
2022-07-01 12:09:57 +0300

    SED-1029 Added new timeseries visualization

---
+++ projects/step-frontend/src/lib/modules/timeseries/ranger/ts-ranger-settings.ts
@@ -0,0 +1,6 @@
+import { TSChartSeries } from '../chart/model/ts-chart-settings';
+
+export interface TSRangerSettings {
+  xValues: number[];
+  series: TSChartSeries[];
+}
---
+++ projects/step-frontend/src/lib/modules/timeseries/time-series.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FindBucketsRequest } from './find-buckets-request';
+import { TimeSeriesChartResponse } from './time-series-chart-response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TimeSeriesService {
+  constructor(private http: HttpClient) {}
+
+  fetchBuckets(request: FindBucketsRequest): Observable<any> {
+    return this.http.post<any>(`/rest/time-series/buckets`, request);
+  }
+
+  fetchBucketsNew(request: FindBucketsRequest): Observable<TimeSeriesChartResponse> {
+    return this.http.post<TimeSeriesChartResponse>(`/rest/time-series/buckets-new`, request);
+  }
+
+  getExecutionDetails(id: string) {
+    return this.http.get<any>(`/rest/executions/${id}`);
+  }
+}
