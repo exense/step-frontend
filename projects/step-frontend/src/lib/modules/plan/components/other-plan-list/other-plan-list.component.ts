e7643d353e56a93fa9053853599b8b9558f26d44
Gilbert Fritz <gilbert.fritz@exense.ch>
2022-07-13 17:08:22 +0200

    SED-1048 cleanup and fix name search column

--- projects/step-frontend/src/lib/modules/plan/components/other-plan-list/other-plan-list.component.ts
+++ projects/step-frontend/src/lib/modules/plan/components/other-plan-list/other-plan-list.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { downgradeComponent, getAngularJSGlobal } from '@angular/upgrade/static';
 import { AJS_LOCATION, AJS_MODULE, AugmentedPlansService, Plan } from '@exense/step-core';
-import { PlanDialogsService } from '../../servies/plan-dialogs.service';
 import { ILocationService } from 'angular';
 
 @Component({
@@ -11,30 +10,16 @@ import { ILocationService } from 'angular';
   styleUrls: ['./other-plan-list.component.scss'],
 })
 export class OtherPlanListComponent {
-  /*
-  @Input()
-  addPlan: (id: string) => void;
-*/
-
   @Output() onSelection = new EventEmitter<string>();
 
   constructor(
     readonly _plansApiService: AugmentedPlansService,
-    private _planDialogs: PlanDialogsService,
     private _httpClient: HttpClient,
     @Inject(AJS_LOCATION) private _location: ILocationService
   ) {}
 
   addPlan(id: string): void {
     this.onSelection.emit(id);
-    // this._httpClient.get<any>('rest/plans/' + id).subscribe((plan) => {
-    //   this._httpClient.get<any>('rest/plans/artefact/types/CallPlan').subscribe((newArtefact) => {
-    //     newArtefact.attributes.name = plan.attributes.name;
-    //     newArtefact.dynamicName.dynamic = newArtefact.useDynamicName;
-    //     newArtefact.planId = id;
-    //     // addArtefactToCurrentNode(newArtefact); // todo
-    //   });
-    // });
   }
 }
 
