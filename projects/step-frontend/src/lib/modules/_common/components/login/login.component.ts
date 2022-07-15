85db9959c8a0bab7cb618838267360a75a5cc0a8
Tim Rasim <neogucky@gmail.com>
2022-07-15 10:45:23 +0200

    SED-1140 Created new login page

---
+++ projects/step-frontend/src/lib/modules/_common/components/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { downgradeComponent, getAngularJSGlobal } from '@angular/upgrade/static';
+import { AJS_MODULE, AuthService } from '@exense/step-core';
+
+@Component({
+  selector: 'step-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  credentials = {
+    username: '',
+    password: '',
+  };
+  error?: string;
+  capsLock?: boolean;
+
+  @Input() logo: string = 'images/logologin.png';
+
+  constructor(private _auth: AuthService) {}
+
+  ngOnInit() {
+    if (this._auth.getConf()?.demo) {
+      this.credentials.password = 'init';
+      this.credentials.username = 'admin';
+    }
+  }
+
+  login() {
+    console.log(this.credentials);
+    this._auth.login(this.credentials).then(
+      function (user) {},
+      (e) => (this.error = e.error)
+    );
+  }
+}
+
+getAngularJSGlobal()
+  .module(AJS_MODULE)
+  .directive('stepLogin', downgradeComponent({ component: LoginComponent }));
