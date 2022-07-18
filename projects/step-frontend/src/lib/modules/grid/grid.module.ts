c0283b40ae9e7dde915dc8503bbb9bee3451283d
Gilbert Fritz <gilbert.fritz@exense.ch>
2022-07-18 17:24:11 +0200

    SED-1053 renamed dir services to pipes

--- projects/step-frontend/src/lib/modules/grid/grid.module.ts
+++ projects/step-frontend/src/lib/modules/grid/grid.module.ts
@@ -5,7 +5,7 @@ import { AgentListComponent } from './components/agent-list/agent-list.component
 import { TokenStateProgressbarComponent } from './components/token-state-progressbar/token-state-progressbar.component';
 import { TokenListComponent } from './components/token-list/token-list.component';
 import { CalcStatusSummaryProgressbarPercentPipe } from './components/token-state-progressbar/calc-status-summary-progressbar-with-percent.pipe';
-import { FlatObjectStringFormatPipe } from './services/flat-object-format.pipe';
+import { FlatObjectStringFormatPipe } from './pipes/flat-object-format.pipe';
 import { AddPrefixPipe } from './components/token-type/add-prefix.pipe';
 import { TokenTypeComponent } from './components/token-type/token-type.component';
 import { TokenStateComponent } from './components/token-state/token-state.component';
