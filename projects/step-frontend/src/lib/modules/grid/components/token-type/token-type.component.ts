2dc64439b396496545ac7ed4e49791c48fe3b313
Gilbert Fritz <gilbert.fritz@exense.ch>
2022-07-18 16:50:12 +0200

    SED-1052 implemented review feedback

---
+++ projects/step-frontend/src/lib/modules/grid/components/token-type/token-type.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'step-token-type',
+  templateUrl: './token-type.component.html',
+  styleUrls: ['./token-type.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TokenTypeComponent {
+  @Input() type!: string;
+
+  static readonly TYPE_LABEL_TRANSLATIONS: { [key: string]: string } = {
+    default: 'Java',
+    node: 'Node.js',
+    dotnet: '.NET',
+  };
+
+  readonly typeLabelTranslations = TokenTypeComponent.TYPE_LABEL_TRANSLATIONS;
+
+  constructor() {}
+}
--- projects/step-frontend/src/lib/modules/grid/grid.module.ts
+++ projects/step-frontend/src/lib/modules/grid/grid.module.ts
@@ -2,18 +2,26 @@ import { NgModule } from '@angular/core';
 import { StepCommonModule } from '../_common/step-common.module';
 import { StepCoreModule } from '@exense/step-core';
 import { AgentListComponent } from './components/agent-list/agent-list.component';
-import { StatusSummaryComponent } from './components/status-summary/status-summary.component';
+import { TokenStateProgressbarComponent } from './components/token-state-progressbar/token-state-progressbar.component';
 import { TokenListComponent } from './components/token-list/token-list.component';
-import { CalcStatusSummaryProgressbarWithPercentPipe } from './components/status-summary/calc-status-summary-progressbar-with-percent.pipe';
+import { CalcStatusSummaryProgressbarPercentPipe } from './components/token-state-progressbar/calc-status-summary-progressbar-with-percent.pipe';
+import { FlatObjectStringFormatPipe } from './services/flat-object-format.pipe';
+import { AddPrefixPipe } from './components/token-type/add-prefix.pipe';
+import { TokenTypeComponent } from './components/token-type/token-type.component';
+import { TokenStateComponent } from './components/token-state/token-state.component';
 
 @NgModule({
   declarations: [
     AgentListComponent,
     TokenListComponent,
-    StatusSummaryComponent,
-    CalcStatusSummaryProgressbarWithPercentPipe,
+    TokenStateProgressbarComponent,
+    TokenTypeComponent,
+    TokenStateComponent,
+    CalcStatusSummaryProgressbarPercentPipe,
+    FlatObjectStringFormatPipe,
+    AddPrefixPipe,
   ],
-  exports: [AgentListComponent, TokenListComponent, StatusSummaryComponent],
+  exports: [AgentListComponent, TokenListComponent, TokenStateProgressbarComponent],
   imports: [StepCoreModule, StepCommonModule],
 })
 export class GridModule {}
