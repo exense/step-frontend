cb68245bd0c7d06984c212512aa0ebf1a2e516f7
Gilbert Fritz <gilbert.fritz@exense.ch>
2022-08-16 11:57:35 +0200

    SED-1169 regenerated client on fixed specs 2

---
+++ projects/step-core/src/lib/client/generated/models/OQLFilter.ts
@@ -0,0 +1,10 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { TableFilter } from './TableFilter';
+
+export type OQLFilter = (TableFilter & {
+    oql?: string;
+});
+
---
+++ projects/step-core/src/lib/client/generated/services/PrivateViewPluginService.ts
@@ -0,0 +1,36 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import { Injectable } from '@angular/core';
+import type { Observable } from 'rxjs';
+
+import type { ViewModel } from '../models/ViewModel';
+
+import { BaseHttpRequest } from '../core/BaseHttpRequest';
+
+@Injectable({providedIn:'root'})
+export class PrivateViewPluginService {
+
+    constructor(public readonly httpRequest: BaseHttpRequest) {}
+
+    /**
+     * @param id
+     * @param executionId
+     * @returns ViewModel default response
+     * @throws ApiError
+     */
+    public getView(
+        id: string,
+        executionId: string,
+    ): Observable<ViewModel> {
+        return this.httpRequest.request({
+            method: 'GET',
+            url: '/views/{id}/{executionId}',
+            path: {
+                'id': id,
+                'executionId': executionId,
+            },
+        });
+    }
+
+}
