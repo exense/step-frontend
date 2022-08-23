4891fb0a704c23008c7a7a52600f4c27ea5957a2
Vladimir <dragonvladir@gmail.com>
2022-08-23 09:28:57 +0000

    SED-987: Adapt legacy tables to new API
    * SED-987: Adapt legacy tables to new API
    
    * SED-987-adapt-legacy-tables (Remove legacy request)
    
    * Fixed issue with opening function editor
    
    * SED-987: Adapt legacy tables to new API fixes

--- projects/step-core/src/lib/core-initialiser.ts
+++ projects/step-core/src/lib/core-initialiser.ts
@@ -1,10 +1,11 @@
 import { APP_INITIALIZER, FactoryProvider } from '@angular/core';
 import { getAngularJSGlobal } from '@angular/upgrade/static';
-import { AJS_MODULE } from './shared';
+import { AJS_MODULE, TABLES } from './shared';
 import { STEP_CORE_JS } from './angularjs';
 
 const addCoreAngularDependency = () => async () => {
   getAngularJSGlobal().module(AJS_MODULE).requires.push(STEP_CORE_JS);
+  getAngularJSGlobal().module(TABLES).requires.push(STEP_CORE_JS);
 };
 
 export const CORE_INITIALIZER: FactoryProvider = {
