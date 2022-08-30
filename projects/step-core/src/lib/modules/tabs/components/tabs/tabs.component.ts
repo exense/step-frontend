02553a33f94f2c4462ad1690f7d98e906f264b76
Vladimir Adamov <vladimir@logicflow.ai>
2022-08-30 16:02:51 +0000

    SED-1186: Ability to display tabbar both as tabs and buttons

--- projects/step-core/src/lib/modules/tabs/components/tabs/tabs.component.ts
+++ projects/step-core/src/lib/modules/tabs/components/tabs/tabs.component.ts
@@ -1,4 +1,15 @@
-import { Component, EventEmitter, Input, Output, TemplateRef, TrackByFunction, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  TemplateRef,
+  TrackByFunction,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Tab } from '../../shared/tab';
 import { AJS_MODULE } from '../../../../shared';
 import { downgradeComponent, getAngularJSGlobal } from '@angular/upgrade/static';
@@ -9,14 +20,27 @@ import { downgradeComponent, getAngularJSGlobal } from '@angular/upgrade/static'
   styleUrls: ['./tabs.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class TabsComponent {
+export class TabsComponent implements OnChanges {
   readonly trackByTab: TrackByFunction<Tab> = (index, item) => item.id;
 
+  @Input() tabMode: 'buttons' | 'tabs' = 'buttons';
   @Input() disablePagination: boolean = false;
   @Input() tabs: Tab[] = [];
   @Input() activeTabId?: string;
   @Input() tabTemplate?: TemplateRef<unknown>;
   @Output() activeTabIdChange = new EventEmitter<string>();
+
+  @HostBinding('class.tab-mode-buttons') isTabModeButtons: boolean = true;
+  @HostBinding('class.tab-mode-tabs') isTabModeTabs: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const cTabMode = changes['tabMode'];
+    if (!!cTabMode?.currentValue && (cTabMode?.previousValue !== cTabMode?.currentValue || cTabMode?.firstChange)) {
+      const tabMode = cTabMode.currentValue;
+      this.isTabModeButtons = tabMode === 'button';
+      this.isTabModeTabs = tabMode === 'tabs';
+    }
+  }
 
   selectTab(tab: Tab): void {
     this.activeTabId = tab.id;
