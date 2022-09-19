99a735a61a631f99b0712dbb958760a3a914a083
Vladimir Adamov <vladimir@logicflow.ai>
2022-09-19 16:03:53 +0000

    SED-1240 Feather icons. Change icon's size.

--- projects/step-core/src/lib/modules/step-icons/services/icon-provider.service.ts
+++ projects/step-core/src/lib/modules/step-icons/services/icon-provider.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { allIcons } from '../icons';
+import { downgradeInjectable, getAngularJSGlobal } from '@angular/upgrade/static';
+import { STEP_CORE_JS } from '../../../angularjs';
 
 const STRING_CAMELIZE_REGEXP = /(-|_|\.|\s)+(.)?/g;
 
-const camelize = (str: string) => str
-  .replace(STRING_CAMELIZE_REGEXP, (_match, _separator, chr) => {
-    return chr ? chr.toUpperCase() : '';
-  })
-  .replace(/^([A-Z])/, (match) => match.toLowerCase());
+const camelize = (str: string) =>
+  str
+    .replace(STRING_CAMELIZE_REGEXP, (_match, _separator, chr) => {
+      return chr ? chr.toUpperCase() : '';
+    })
+    .replace(/^([A-Z])/, (match) => match.toLowerCase());
 
 const upperCamelize = (str: string) => {
   const camelCase = camelize(str);
@@ -15,15 +18,17 @@ const upperCamelize = (str: string) => {
 };
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IconProviderService {
   getIcon(key: string): string {
-     const iconName = upperCamelize(key);
-     const svg = (allIcons as any)[iconName] || '';
-     if (!svg) {
-       console.warn(`Icon not found ${key}`);
-     }
-     return svg;
+    const iconName = upperCamelize(key);
+    const svg = (allIcons as any)[iconName] || '';
+    if (!svg) {
+      console.warn(`Icon not found ${key}`);
+    }
+    return svg;
   }
 }
+
+getAngularJSGlobal().module(STEP_CORE_JS).service('iconProverService', downgradeInjectable(IconProviderService));
