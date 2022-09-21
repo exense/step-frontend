b820e78095968573b85258d9799c80314bb8e88b
Vladimir Adamov <vladimir@logicflow.ai>
2022-09-21 14:03:08 +0000

    SED-1288 Show error for invalid date

--- projects/step-core/src/lib/components/date-filter/date-filter.component.ts
+++ projects/step-core/src/lib/components/date-filter/date-filter.component.ts
@@ -1,17 +1,50 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { DateTime } from 'luxon';
+import { AbstractControl, FormBuilder, FormControl, ValidatorFn, Validators } from '@angular/forms';
+import { debounceTime, map, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'step-date-filter',
   templateUrl: './date-filter.component.html',
   styleUrls: ['./date-filter.component.scss'],
 })
-export class DateFilterComponent {
+export class DateFilterComponent implements OnInit, OnDestroy {
+  private terminator$ = new Subject<unknown>();
+
   @Output() dateChanged = new EventEmitter<DateTime | undefined>();
 
-  handleDateChange(event: MatDatepickerInputEvent<DateTime>): void {
-    const date = event?.value || undefined;
-    this.dateChanged.emit(date);
+  @ViewChild('dateInput') private dateInput?: ElementRef;
+
+  readonly dateControl: FormControl;
+
+  constructor(formBuilder: FormBuilder) {
+    this.dateControl = formBuilder.control('', [this.createDateValidator()]);
+  }
+
+  ngOnInit() {
+    this.dateControl.valueChanges
+      .pipe(
+        debounceTime(200),
+        map((value) => value || undefined),
+        takeUntil(this.terminator$)
+      )
+      .subscribe((date) => this.dateChanged.emit(date));
+  }
+
+  ngOnDestroy(): void {
+    this.terminator$.next(undefined);
+    this.terminator$.complete();
+  }
+
+  private createDateValidator(): ValidatorFn {
+    return (control: AbstractControl) => {
+      // If date is null, but input has values - it means that date wasn't parsed
+      if (control.value === null && !!this.dateInput?.nativeElement?.value) {
+        return {
+          date: 'Invalid date',
+        };
+      }
+      return null;
+    };
   }
 }
