0fce7973fcda12725d03f4d14692530d19d5a903
Lucian Baciu <lucian.baciu@exense.ch>
2022-09-27 00:55:13 +0300

    SED-1392-Issues regarding colors
    SED-1346-Scrolling not working property on "Performance" view
    SED-1357-The link from callKeywordReportNode to interactive analysis is broken

--- projects/step-frontend/src/lib/modules/timeseries/performance-view/measurements/measurements-picker.component.ts
+++ projects/step-frontend/src/lib/modules/timeseries/performance-view/measurements/measurements-picker.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TimeSeriesContextsFactory } from '../../time-series-contexts-factory.service';
 import { KeywordSelection, TimeSeriesKeywordsContext } from '../../execution-page/time-series-keywords.context';
 import { KeyValue } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'step-measurements-picker',
   templateUrl: './measurements-picker.component.html',
   styleUrls: ['./measurements-picker.component.scss'],
 })
-export class MeasurementsPickerComponent implements OnInit {
+export class MeasurementsPickerComponent implements OnInit, OnDestroy {
   @Input() contextId!: string;
 
   private keywordsService!: TimeSeriesKeywordsContext;
@@ -17,6 +18,8 @@ export class MeasurementsPickerComponent implements OnInit {
   allSeriesChecked: boolean = true;
   activeKeywords = 0;
 
+  subscriptions: Subscription = new Subscription();
+
   constructor(private executionPageService: TimeSeriesContextsFactory) {}
 
   ngOnInit(): void {
@@ -24,24 +27,28 @@ export class MeasurementsPickerComponent implements OnInit {
       throw new Error('Context Id input must be present');
     }
     this.keywordsService = this.executionPageService.getContext(this.contextId).getKeywordsContext();
-    this.keywordsService.onKeywordsUpdated().subscribe((keywords) => {
-      this.keywords = keywords;
-      this.activeKeywords = 0;
-      Object.keys(keywords).forEach((key) => {
-        if (keywords[key].isVisible && keywords[key].isSelected) {
+    this.subscriptions.add(
+      this.keywordsService.onKeywordsUpdated().subscribe((keywords) => {
+        this.keywords = keywords;
+        this.activeKeywords = 0;
+        Object.keys(keywords).forEach((key) => {
+          if (keywords[key].isVisible && keywords[key].isSelected) {
+            this.activeKeywords++;
+          }
+        });
+      })
+    );
+    this.subscriptions.add(
+      this.keywordsService.onKeywordToggled().subscribe((selection) => {
+        let isSelected = this.keywords[selection.id].isSelected;
+        if (isSelected) {
           this.activeKeywords++;
+        } else {
+          this.allSeriesChecked = false;
+          this.activeKeywords--;
         }
-      });
-    });
-    this.keywordsService.onKeywordToggled().subscribe((selection) => {
-      let isSelected = this.keywords[selection.id].isSelected;
-      if (isSelected) {
-        this.activeKeywords++;
-      } else {
-        this.allSeriesChecked = false;
-        this.activeKeywords--;
-      }
-    });
+      })
+    );
   }
 
   onAllSeriesCheckboxClick(event: any) {
@@ -60,4 +67,10 @@ export class MeasurementsPickerComponent implements OnInit {
   valueAscOrder = (a: KeyValue<string, any>, b: KeyValue<string, any>): number => {
     return a.key.localeCompare(b.key);
   };
+
+  ngOnDestroy(): void {
+    if (this.subscriptions) {
+      this.subscriptions.unsubscribe();
+    }
+  }
 }
