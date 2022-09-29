60f45987b1fc46238527b432691189d5e6d231a8
Vladimir <dragonvladir@gmail.com>
2022-09-29 11:11:14 +0000

    SED-1241 Replace old icons
    * SED-1241 Experiment with tree icons
    
    * SED-1241 Replace icons
    
    * SED-1241 replace old icons
    
    * SED-1241 reduce icons size

--- projects/step-core/src/lib/angularjs/directives/step-icon.directive.ts
+++ projects/step-core/src/lib/angularjs/directives/step-icon.directive.ts
@@ -1,12 +1,26 @@
 import { IScope, IDirectiveFactory, Injectable, IAttributes } from 'angular';
 
+export const STEP_ICON_DIRECTIVE_NAME = 'stepAjsIcon';
+export const STEP_ICON_CLASS = 'step-ajs-icon';
+
 export const STEP_ICON_DIRECTIVE: Injectable<IDirectiveFactory> = [
   'iconProverService',
   (iconProverService: any) => ({
-    restrict: 'E',
+    restrict: 'EC',
     template: '',
     link: function ($scope: IScope, $el: JQLite, $attr: IAttributes) {
-      const name = ($attr as any).name;
+      const resolveName = () => {
+        let name = ($attr as any).name;
+        if (name) {
+          return name;
+        }
+        const classValueParts = ($el[0].className || '').split(' ');
+        const index = classValueParts.indexOf(STEP_ICON_CLASS);
+        name = classValueParts[index + 1];
+        return name;
+      };
+
+      const name = resolveName();
       const icon = iconProverService.getIcon(name);
       if (icon) {
         $el.html(icon);
