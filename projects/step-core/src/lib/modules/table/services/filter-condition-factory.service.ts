a82b4a60363aade79c064eaa6d5dc2aa8e3477fe
Vladimir Adamov <vladimir@logicflow.ai>
2022-10-04 14:11:05 +0000

    SED-1410 Search by "Step" not working on KW Calls table

--- projects/step-core/src/lib/modules/table/services/filter-condition-factory.service.ts
+++ projects/step-core/src/lib/modules/table/services/filter-condition-factory.service.ts
@@ -3,6 +3,7 @@ import { DateTime } from 'luxon';
 import { FilterCondition } from '../shared/filter-condition';
 import { SingleDateFilterCondition } from '../shared/single-date-filter-condition';
 import { ScopeFilterCondition } from '../shared/scope-filter-condition';
+import { ReportNodeFilterCondition } from '../shared/report-node-filter-condition';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +15,9 @@ export class FilterConditionFactoryService {
 
   scopeFilterCondition(value?: string): FilterCondition {
     return new ScopeFilterCondition(value);
+  }
+
+  reportNodeFilterCondition(value?: string, attributes?: string[]): FilterCondition {
+    return new ReportNodeFilterCondition(value, attributes);
   }
 }
