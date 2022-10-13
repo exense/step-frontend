b8a7d8b191b4c910e0a961e689d337d4014fa7de
Tim Rasim <neogucky@gmail.com>
2022-10-13 17:09:01 +0200

    SED-1466 Added functionality to open regularly used menus by default

--- projects/step-frontend/src/lib/modules/_common/components/sidebar/sidebar.component.ts
+++ projects/step-frontend/src/lib/modules/_common/components/sidebar/sidebar.component.ts
@@ -49,6 +49,7 @@ export class SidebarComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this.openMainMenuBasedOnActualView();
+    this.openEssentialMainMenus();
   }
 
   ngOnDestroy(): void {
@@ -63,6 +64,15 @@ export class SidebarComponent implements AfterViewInit, OnDestroy {
         this.openMainMenu(initiallyExpandedMainMenuKey);
       }
     }
+  }
+
+  private openEssentialMainMenus(): void {
+    const essentialMenuWeightThreshold = 20;
+    this._viewRegistryService.getMainMenuAll().forEach((menu: MenuEntry) => {
+      if (menu.weight && menu.weight <= essentialMenuWeightThreshold) {
+        this.openMainMenu(menu.viewId);
+      }
+    });
   }
 
   private openMainMenu(mainMenuKey: string): void {
