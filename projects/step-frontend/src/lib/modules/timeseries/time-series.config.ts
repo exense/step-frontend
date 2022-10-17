8407c032359d94ebcce325f2517bfc42f43f19dc
Lucian Baciu <103116491+lucianbaciu@users.noreply.github.com>
2022-10-17 19:54:35 +0300

    Sed 1443 ts   missing legend for total throughput on  throughput  dashlet
    * SED-1443-Added 'total' information in tooltip
    
    * SED-1443-Configurable z-scale label in tooltip
    
    * SED-1443-Move color property in config + remove weird unused import

--- projects/step-frontend/src/lib/modules/timeseries/time-series.config.ts
+++ projects/step-frontend/src/lib/modules/timeseries/time-series.config.ts
@@ -2,4 +2,5 @@ export class TimeSeriesConfig {
   public static readonly MAX_BUCKETS_IN_CHART = 100;
 
   public static readonly RESPONSE_TIME_CHART_TITLE = 'Response Times';
+  static readonly SUMMARY_BARS_COLOR = '#97a7d7';
 }
