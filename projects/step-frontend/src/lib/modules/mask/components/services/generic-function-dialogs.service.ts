7e4da386491663608c38463e26be9babf0f291b4
Gilbert Fritz <gilbert.fritz@exense.ch>
2022-10-24 13:11:59 +0200

    SED-1061 cleanup

--- projects/step-frontend/src/lib/modules/mask/components/services/generic-function-dialogs.service.ts
+++ projects/step-frontend/src/lib/modules/mask/components/services/generic-function-dialogs.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { a1Promise2Observable, DialogsService, AugmentedKeywordsService } from '@exense/step-core';
 import { Observable, catchError, map, of, switchMap, tap } from 'rxjs';
 import { FunctionDialogsService } from '../../../function/services/function-dialogs.service';
-import { IsUsedByDialogsService } from '../../../_common/services/is-used-by-dialogs.service';
-import { IsUsedByType } from '../../../_common/shared/is-used-by-type.enum';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +11,6 @@ export class GenericFunctionDialogService {
 
   constructor(
     private _functionDialogs: FunctionDialogsService,
-    private _isUsedByDialogs: IsUsedByDialogsService,
     private _dialogs: DialogsService,
     private _augmentedKeywordsService: AugmentedKeywordsService
   ) {}
@@ -34,10 +31,6 @@ export class GenericFunctionDialogService {
 
   openEditMaskDialog(id: string): void {
     this._functionDialogs.openFunctionEditor(id, this.config);
-  }
-
-  openLookupDialog(id: string, name: string): Observable<any> {
-    return this._isUsedByDialogs.displayDialog(`Keyword "${name}" is used by`, IsUsedByType.KEYWORD_ID, id);
   }
 
   openConfigDialog(id: string): Observable<any> {
