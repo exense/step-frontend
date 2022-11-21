9fa45f0b834ed80781118da5e774481503c2ff43
bojidaryovchev <bozhidar.yovchev@exense.ch>
2022-11-21 16:28:32 +0000

    SED-1078 post-merge fixes

--- projects/step-frontend/src/lib/modules/_common/step-common.module.ts
+++ projects/step-frontend/src/lib/modules/_common/step-common.module.ts
@@ -1,3 +1,4 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { StepCoreModule } from '@exense/step-core';
 import { AutorefreshToggleComponent } from './components/autorefresh-toggle/autorefresh-toggle.component';
@@ -7,19 +8,18 @@ import { JsonViewerDirective } from './directives/json-viewer.directive';
 import { IsEmptyJsonPipe } from './pipes/is-empty-json.pipe';
 import { ContainsVersionPipe } from './pipes/contains-version.pipe';
 import { MenuFilterPipe } from './pipes/menu-filter.pipe';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { IsUsedByListComponent } from './components/is-used-by-list/is-used-by-list.component';
 
-import { FunctionLinkComponent } from './components/function-link/function-link.component';
+import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by-modal.component';
 import { LoginComponent } from './components/login/login.component';
 import { ModalWindowComponent } from './components/modal-window/modal-window.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { VersionsDialogComponent } from './components/versions-dialog/versions-dialog.component';
 import { ArtefactDetailsDirective } from './directives/artefact-details.directive';
 import { CustomFormDirective } from './directives/custom-form.directive';
 import { PlanExecutionDirective } from './directives/plan-execution.directive';
-import { VersionsDialogComponent } from './components/versions-dialog/versions-dialog.component';
-import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by-modal.component';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +28,6 @@ import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by
     SidebarComponent,
     ExecutionLinkComponent,
     IsUsedByListComponent,
-    FunctionLinkComponent,
     ModalWindowComponent,
     JsonViewerDirective,
     IsEmptyJsonPipe,
@@ -47,7 +46,6 @@ import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by
     SidebarComponent,
     ExecutionLinkComponent,
     IsUsedByListComponent,
-    FunctionLinkComponent,
     ModalWindowComponent,
     JsonViewerDirective,
     IsEmptyJsonPipe,
@@ -59,11 +57,17 @@ import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by
     IsUsedByModalComponent,
   ],
   imports: [StepCoreModule],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class StepCommonModule {}
 
-export * from './shared/status.enum';
-export * from './shared/is-used-by-search-type';
-export * from './services/is-used-by-dialog.service';
 export * from './components/function-link/function-link-dialog.service';
+export * from './services/is-used-by-dialog.service';
+export * from './shared/is-used-by-search-type';
+export * from './shared/status.enum';
