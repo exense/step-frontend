fe34d0d38908656087feb4681e7bf60c485997dc
bojidaryovchev <bozhidar.yovchev@exense.ch>
2022-11-21 16:28:32 +0000

    SED-1078 post-merge fixes

--- projects/step-frontend/src/lib/modules/_common/step-common.module.ts
+++ projects/step-frontend/src/lib/modules/_common/step-common.module.ts
@@ -1,23 +1,23 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { AuthService, StepCoreModule } from '@exense/step-core';
+import { StepCoreModule } from '@exense/step-core';
 import { AutorefreshToggleComponent } from './components/autorefresh-toggle/autorefresh-toggle.component';
 import { ExecutionLinkComponent } from './components/execution-link/execution-link.component';
-import { ContainsVersionPipe } from './pipes/contains-version.pipe';
 import { FunctionLinkComponent } from './components/function-link/function-link.component';
 import { IsUsedByListComponent } from './components/is-used-by-list/is-used-by-list.component';
+import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by-modal.component';
 import { LoginComponent } from './components/login/login.component';
 import { ModalWindowComponent } from './components/modal-window/modal-window.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { VersionsDialogComponent } from './components/versions-dialog/versions-dialog.component';
 import { ArtefactDetailsDirective } from './directives/artefact-details.directive';
 import { CustomFormDirective } from './directives/custom-form.directive';
 import { JsonViewerDirective } from './directives/json-viewer.directive';
 import { PlanExecutionDirective } from './directives/plan-execution.directive';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { ContainsVersionPipe } from './pipes/contains-version.pipe';
 import { IsEmptyJsonPipe } from './pipes/is-empty-json.pipe';
 import { MenuFilterPipe } from './pipes/menu-filter.pipe';
-import { VersionsDialogComponent } from './components/versions-dialog/versions-dialog.component';
-import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by-modal.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -56,11 +56,17 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
     IsUsedByModalComponent,
   ],
   imports: [StepCoreModule],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class StepCommonModule {}
 
-export * from './shared/status.enum';
-export * from './shared/is-used-by-search-type';
-export * from './services/is-used-by-dialog.service';
 export * from './components/function-link/function-link-dialog.service';
+export * from './services/is-used-by-dialog.service';
+export * from './shared/is-used-by-search-type';
+export * from './shared/status.enum';
