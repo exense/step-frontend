fcd28f0a95aa4a669a90d2da5c64d79c463f9720
Vladimir Adamov <vladimir@logicflow.ai>
2023-01-17 12:36:42 +0000

    SED-1082 Move zIndex assign logic to separate directive

--- projects/step-core/src/lib/components/modal-window/modal-window.component.ts
+++ projects/step-core/src/lib/components/modal-window/modal-window.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Input, OnDestroy } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -6,34 +6,8 @@ import { MatDialogRef } from '@angular/material/dialog';
   templateUrl: './modal-window.component.html',
   styleUrls: ['./modal-window.component.scss'],
 })
-export class ModalWindowComponent implements AfterViewInit, OnDestroy {
+export class ModalWindowComponent {
   @Input() title: string = '';
 
-  /**
-   * Sometimes it's require to show the migrated modal above the legacy one
-   * This input can be used to specify the z-index for .cdk-overlay-container when modal appears
-   * As soon as all modals will be migrated, this logic should be removed
-   */
-  @Input() zIndex?: number;
-  private cdkOverlayContainer?: HTMLElement;
-  constructor(private _elRef: ElementRef<HTMLElement>, public _dialogRef: MatDialogRef<unknown>) {}
-
-  ngAfterViewInit(): void {
-    if (this.zIndex === undefined) {
-      return;
-    }
-    this.cdkOverlayContainer = this._elRef.nativeElement.closest<HTMLElement>('.cdk-overlay-container') || undefined;
-    if (!this.cdkOverlayContainer) {
-      return;
-    }
-    this.cdkOverlayContainer.style.zIndex = this.zIndex.toString();
-  }
-
-  ngOnDestroy(): void {
-    if (!this.cdkOverlayContainer) {
-      return;
-    }
-    this.cdkOverlayContainer.style.removeProperty('z-index');
-    this.cdkOverlayContainer = undefined;
-  }
+  constructor(public _dialogRef: MatDialogRef<unknown>) {}
 }
