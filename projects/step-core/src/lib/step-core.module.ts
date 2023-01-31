54fc2177058b01aafbe36e6b99cbc3882bf9a6d9
bojidaryovchev <blizzardcon10@gmail.com>
2023-01-31 18:12:39 +0200

    SED-1702 Update plan editor content-split
    * SED-1668 Plan basic settings redesign
    
    - custom-form.component migration
    - custom-form-input.component migration
    
    * SED-1668 Plan basic settings redesign
    
    - editable-actions.component styling improvements
    - editable-dropdown-label.component zIndex problem inside old dialogs
    - z-index.directive bugfix regarding element selection
    - general layout improvements (flexbox instead of calc(), etc.)
    
    * SED-1668 Plan basic settings redesign
    
    * SED-1668 post-merge fixes
    
    * SED-1668 post-merge fixes
    
    - styling fixes
    
    * SED-1702 Update plan editor content-split
    
    - introduced custom split components and applied them to the plan editor and plan tree
    
    * SED-1668 Plan basic settings redesign
    
    - minor styling fixes
    - uncommented something forgotten
    
    * SED-1668 resolving PR comments
    
    - moved table-related styles to _step-table.scss
    - improved the z-index.directive
    - editable label components can now accept a tooltip
    
    * SED-1668 reference changing reverted, pipe made impure for the time being
    
    * SED-1702 post-merge fixes

--- projects/step-core/src/lib/step-core.module.ts
+++ projects/step-core/src/lib/step-core.module.ts
@@ -13,24 +13,34 @@ import { CustomFormComponent } from './components/custom-form/custom-form.compon
 import { EditableActionsComponent } from './components/editable-actions/editable-actions.component';
 import { EditableDropdownLabelComponent } from './components/editable-dropdown-label/editable-dropdown-label.component';
 import { EditableLabelComponent } from './components/editable-label/editable-label.component';
+import { EditableTextareaLabelWrapperComponent } from './components/editable-textarea-label-wrapper/editable-textarea-label-wrapper.component';
 import { EditableTextareaLabelComponent } from './components/editable-textarea-label/editable-textarea-label.component';
 import { FunctionLinkComponent } from './components/function-link/function-link.component';
 import { IsUsedByListComponent } from './components/is-used-by-list/is-used-by-list.component';
 import { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by-modal.component';
+import { KeywordNameComponent } from './components/keyword-name/keyword-name.component';
 import { ModalWindowComponent } from './components/modal-window/modal-window.component';
 import { PlanLinkComponent } from './components/plan-link/plan-link.component';
 import { PlanTreeActionsComponent } from './components/plan-tree-actions/plan-tree-actions.component';
+import { PlanTreeComponent } from './components/plan-tree/plan-tree.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { ReportNodeStatusComponent } from './components/report-node-status/report-node-status.component';
+import { RestoreDialogComponent } from './components/restore-dialog/restore-dialog.component';
 import { SelectPlanComponent } from './components/select-plan/select-plan.component';
+import { SettingBtnComponent } from './components/setting-btn/setting-btn.component';
+import { SplitAreaComponent } from './components/split-area/split-area.component';
+import { SplitGutterComponent } from './components/split-gutter/split-gutter.component';
+import { SplitComponent } from './components/split/split.component';
 import { UploadContainerComponent } from './components/upload-container/upload-container.component';
 import { CORE_INITIALIZER } from './core-initialiser';
+import { ArtefactDetailsDirective } from './directives/artefact-details.directive';
 import { CapsLockDirective } from './directives/caps-lock.directive';
 import { SimpleLineChartDirective } from './directives/simple-line-chart.directive';
 import { TooltipImmediateCloseDirective } from './directives/tooltip-immediate-close.directive';
 import { TooltipDirective } from './directives/tooltip.directive';
 import { StepBasicsModule } from './modules/basics/step-basics.module';
 import { CustomRegistriesModule } from './modules/custom-registeries/custom-registries.module';
+import { DynamicFormsModule } from './modules/dynamic-forms/dynamic-forms.module';
 import { EntitiesSelectionModule } from './modules/entities-selection/entities-selection.module';
 import { EntityModule } from './modules/entity/entity.module';
 import { StepMaterialModule } from './modules/step-material/step-material.module';
@@ -39,17 +49,10 @@ import { TabsModule } from './modules/tabs/tabs.module';
 import { TreeModule } from './modules/tree/tree.module';
 import { CustomFormInputModelPipe } from './pipes/custom-form-input-model.pipe';
 import { DashboardLinkPipe } from './pipes/dashboard-link.pipe';
+import { DynamicAttributePipe } from './pipes/dynamic-attribute.pipe';
 import { IsChartEmptyPipe } from './pipes/is-chart-empty.pipe';
 import { MatchingAuthenticator } from './pipes/matching-authenticator.pipe';
 import { PlanNamePipe } from './pipes/plan-name.pipe';
-import { SettingBtnComponent } from './components/setting-btn/setting-btn.component';
-import { PlanTreeComponent } from './components/plan-tree/plan-tree.component';
-import { ArtefactDetailsDirective } from './directives/artefact-details.directive';
-import { RestoreDialogComponent } from './components/restore-dialog/restore-dialog.component';
-import { DynamicFormsModule } from './modules/dynamic-forms/dynamic-forms.module';
-import { KeywordNameComponent } from './components/keyword-name/keyword-name.component';
-import { DynamicAttributePipe } from './pipes/dynamic-attribute.pipe';
-import { EditableTextareaLabelWrapperComponent } from './components/editable-textarea-label-wrapper/editable-textarea-label-wrapper.component';
 
 @NgModule({
   declarations: [
@@ -86,6 +89,9 @@ import { EditableTextareaLabelWrapperComponent } from './components/editable-tex
     CustomFormComponent,
     CustomFormInputComponent,
     CustomFormInputModelPipe,
+    SplitComponent,
+    SplitAreaComponent,
+    SplitGutterComponent,
   ],
   imports: [
     CommonModule,
@@ -150,6 +156,9 @@ import { EditableTextareaLabelWrapperComponent } from './components/editable-tex
     CustomFormComponent,
     CustomFormInputComponent,
     EditableTextareaLabelWrapperComponent,
+    SplitComponent,
+    SplitAreaComponent,
+    SplitGutterComponent,
   ],
   providers: [
     CORE_INITIALIZER,
@@ -187,8 +196,8 @@ export { CustomFormInputComponent } from './components/custom-form-input/custom-
 export { CustomFormComponent } from './components/custom-form/custom-form.component';
 export { EditableDropdownLabelComponent } from './components/editable-dropdown-label/editable-dropdown-label.component';
 export { EditableLabelComponent } from './components/editable-label/editable-label.component';
-export { EditableTextareaLabelComponent } from './components/editable-textarea-label/editable-textarea-label.component';
 export { EditableTextareaLabelWrapperComponent } from './components/editable-textarea-label-wrapper/editable-textarea-label-wrapper.component';
+export { EditableTextareaLabelComponent } from './components/editable-textarea-label/editable-textarea-label.component';
 export { FunctionLinkDialogService } from './components/function-link/function-link-dialog.service';
 export { FunctionLinkComponent } from './components/function-link/function-link.component';
 export { IsUsedByModalComponent } from './components/is-used-by-modal/is-used-by-modal.component';
@@ -197,10 +206,13 @@ export { ModalWindowComponent } from './components/modal-window/modal-window.com
 export { PlanLinkComponent } from './components/plan-link/plan-link.component';
 export * from './components/plan-tree/plan-tree.component';
 export { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
-export { RestoreDialogComponent } from './components/restore-dialog/restore-dialog.component';
 export * from './components/report-node-status/report-node-status.component';
+export { RestoreDialogComponent } from './components/restore-dialog/restore-dialog.component';
 export { SelectPlanComponent } from './components/select-plan/select-plan.component';
 export { SettingBtnComponent } from './components/setting-btn/setting-btn.component';
+export { SplitAreaComponent } from './components/split-area/split-area.component';
+export { SplitGutterComponent } from './components/split-gutter/split-gutter.component';
+export { SplitComponent } from './components/split/split.component';
 export { UploadContainerComponent } from './components/upload-container/upload-container.component';
 export * from './decorators/plugin';
 export * from './directives/artefact-details.directive';
@@ -212,6 +224,7 @@ export * from './domain';
 export * from './modules/async-operations/async-operations.module';
 export * from './modules/basics/step-basics.module';
 export * from './modules/custom-registeries/custom-registries.module';
+export * from './modules/dynamic-forms/dynamic-forms.module';
 export * from './modules/entities-selection/entities-selection.module';
 export * from './modules/entity/entity.module';
 export * from './modules/step-icons/step-icons.module';
@@ -219,10 +232,9 @@ export * from './modules/step-material/step-material.module';
 export * from './modules/table/table.module';
 export * from './modules/tabs/tabs.module';
 export * from './modules/tree/tree.module';
-export * from './modules/dynamic-forms/dynamic-forms.module';
 export * from './pipes/dashboard-link.pipe';
-export * from './pipes/is-chart-empty.pipe';
 export * from './pipes/dynamic-attribute.pipe';
+export * from './pipes/is-chart-empty.pipe';
 export * from './pipes/matching-authenticator.pipe';
 export { PlanNamePipe } from './pipes/plan-name.pipe';
 export * from './services/additional-right-rule.service';
@@ -239,9 +251,9 @@ export { PlanDialogsService } from './services/plan-dialogs.service';
 export * from './services/plan-editor.service';
 export * from './services/plan-interactive-session.service';
 export * from './services/plugin-info-registry.service';
+export * from './services/restore-dialogs.service';
 export * from './services/scheduled-task-dialogs.service';
 export { UibModalHelperService, UibModalInstance } from './services/uib-modal-helper.service';
 export * from './services/view-registry.service';
 export * from './services/view-state.service';
-export * from './services/restore-dialogs.service';
 export * from './shared';
