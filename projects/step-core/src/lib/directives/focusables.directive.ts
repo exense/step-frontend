048714ff27090e769eb9596675b093d109338b1b
bojidaryovchev <bozhidar.yovchev@exense.ch>
2023-02-05 15:03:53 +0000

    NO-TASK time series filters a11y
    - introduced the focusable.directive
    - introduced the focusables.directive
    - introduced the trap-focus.directive
    - introduced the recursive-tab-index.directive
    
    (cherry picked from commit 55c754020b704c2989a70ff3ac5ed6d5094fa74b)
    
    # Conflicts:
    #	projects/step-core/src/lib/step-core.module.ts
    #	projects/step-core/styles/components/_step-inputs.scss

---
+++ projects/step-core/src/lib/directives/focusables.directive.ts
@@ -0,0 +1,50 @@
+import { AfterContentInit, ContentChildren, Directive, Input, OnDestroy, QueryList } from '@angular/core';
+import { Subject } from 'rxjs';
+import { FocusableDirective } from './focusable.directive';
+
+@Directive({
+  selector: '[stepFocusables]',
+  exportAs: 'stepFocusables',
+})
+export class FocusablesDirective implements AfterContentInit, OnDestroy {
+  @Input() focusIndex: number = 0;
+
+  @ContentChildren(FocusableDirective, { descendants: true }) focusables?: QueryList<FocusableDirective>;
+
+  private readonly terminator$ = new Subject<void>();
+
+  private focusSubjectByIndex?: Map<number, Subject<void>>;
+
+  ngAfterContentInit(): void {
+    if (!this.focusables) {
+      return;
+    }
+
+    this.focusSubjectByIndex = new Map(this.focusables.map((focusable, index) => [index, focusable.focus$]));
+  }
+
+  ngOnDestroy(): void {
+    this.terminator$.next();
+    this.terminator$.complete();
+  }
+
+  refocus(): void {
+    if (!this.focusSubjectByIndex) {
+      return;
+    }
+
+    const focusSubject = this.focusSubjectByIndex.get(this.focusIndex);
+
+    if (!focusSubject) {
+      return;
+    }
+
+    focusSubject.next();
+  }
+
+  scheduleRefocus(milliseconds?: number): void {
+    setTimeout(() => {
+      this.refocus();
+    }, milliseconds);
+  }
+}
