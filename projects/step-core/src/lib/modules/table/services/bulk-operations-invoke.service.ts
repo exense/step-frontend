57bfee8386dfe530a66774419a2c9416aec99464
Vladimir Adamov <vladimir@logicflow.ai>
2023-03-20 16:41:35 +0000

    SED-1721 Make warning more prominent when you're about to delete ALL items in a collection

--- projects/step-core/src/lib/modules/table/services/bulk-operations-invoke.service.ts
+++ projects/step-core/src/lib/modules/table/services/bulk-operations-invoke.service.ts
@@ -11,6 +11,9 @@ import {
   AsyncOperationService,
 } from '../../async-operations/async-operations.module';
 import { AsyncTaskStatus } from '../../../client/augmented/shared/async-task-status';
+import { inject } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { TitleCasePipe } from '@angular/common';
 
 export interface BulkOperationConfig<ID> {
   operationType: BulkOperationType;
@@ -21,7 +24,9 @@ export interface BulkOperationConfig<ID> {
 }
 
 export abstract class BulkOperationsInvokeService<ID> {
-  protected constructor(protected _asyncOperationService: AsyncOperationService) {}
+  protected _sanitizer = inject(DomSanitizer);
+  protected _titleCase = inject(TitleCasePipe);
+  protected _asyncOperationService = inject(AsyncOperationService);
 
   protected abstract invokeDelete?(requestBody?: TableBulkOperationRequest): Observable<AsyncTaskStatus>;
   protected abstract invokeDuplicate?(requestBody?: TableBulkOperationRequest): Observable<AsyncTaskStatus>;
@@ -115,8 +120,13 @@ export abstract class BulkOperationsInvokeService<ID> {
     );
   }
 
-  protected createPreviewTitle(config: BulkOperationConfig<ID>): string {
-    return `Confirm ${config.operationType}`;
+  protected createPreviewTitle(config: BulkOperationConfig<ID>): SafeHtml {
+    const operation = this._titleCase.transform(config.operationType);
+    let title = `Confirm ${operation}`;
+    if (config.operationType === BulkOperationType.delete && config.selectionType === BulkSelectionType.All) {
+      title = `<span class="danger-warning">${title}</span>`;
+    }
+    return this._sanitizer.bypassSecurityTrustHtml(title);
   }
 
   protected createPerformTitle(config: BulkOperationConfig<ID>): string {
@@ -135,21 +145,31 @@ export abstract class BulkOperationsInvokeService<ID> {
     };
   }
 
-  protected createSuccessMessageHandler(config: BulkOperationConfig<ID>): (result?: AsyncTaskStatus) => string {
+  protected createSuccessMessageHandler(config: BulkOperationConfig<ID>): (result?: AsyncTaskStatus) => SafeHtml {
     return (result) => {
       const count = result?.result?.count;
-      return count
+      const message = count
         ? `Bulk operation ${config.operationType} for ${count} item(s) completed`
         : `Bulk operation ${config.operationType} for selected items completed`;
+      return this._sanitizer.bypassSecurityTrustHtml(message);
     };
   }
 
-  protected createSuccessMessagePreviewHandler(config: BulkOperationConfig<ID>): (result?: AsyncTaskStatus) => string {
+  protected createSuccessMessagePreviewHandler(
+    config: BulkOperationConfig<ID>
+  ): (result?: AsyncTaskStatus) => SafeHtml {
     return (result) => {
       const count = result?.result?.count;
-      return count
-        ? `Do you want to ${config.operationType} the ${count} selected item(s)?`
-        : `Do you want to ${config.operationType} all selected items?`;
+      let operation = config.operationType.toString();
+      if (config.operationType === BulkOperationType.delete && config.selectionType === BulkSelectionType.All) {
+        operation = `<strong class="danger-warning">${config.operationType}</strong>`;
+      }
+
+      const message = count
+        ? `Do you want to ${operation} the ${count} selected item(s)?`
+        : `Do you want to ${operation} all selected items?`;
+
+      return this._sanitizer.bypassSecurityTrustHtml(message);
     };
   }
 }
