90a2479c2b08fa758b2011e67bc2cf2a27441646
Vladimir <dragonvladir@gmail.com>
2023-04-03 16:43:00 +0300

    SED-1866 Implement our own component to replace mat form fields
    * SED-1866 Implement our own component to replace mat form fields
    
    * SED-1866 remove leftover
    
    * SED-1866 Tiny style improvement

---
+++ projects/step-core/src/lib/modules/basics/components/form-field/form-field.component.ts
@@ -0,0 +1,16 @@
+import { Component, ContentChild, ViewEncapsulation } from '@angular/core';
+import { NgControl } from '@angular/forms';
+@Component({
+  selector: 'step-form-field',
+  templateUrl: './form-field.component.html',
+  styleUrls: ['./form-field.component.scss'],
+  host: {
+    '[class.ng-invalid]': 'control?.invalid',
+    '[class.ng-touched]': 'control?.touched',
+  },
+  encapsulation: ViewEncapsulation.None,
+})
+export class FormFieldComponent {
+  @ContentChild(NgControl)
+  control?: NgControl;
+}
