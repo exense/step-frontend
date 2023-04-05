6dd1ceec9280f250cadf50637981cd13790f5fdd
Vladimir Adamov <vladimir@logicflow.ai>
2023-04-05 09:00:15 +0000

    SED-1069 Tiny fixes

--- projects/step-core/src/lib/modules/entity/services/entity-dialogs.service.ts
+++ projects/step-core/src/lib/modules/entity/services/entity-dialogs.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable} from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SelectEntityOfTypeComponent } from '../components/select-entity-of-type/select-entity-of-type.component';
 import { SelectEntityOfTypeData } from '../types/select-entity-of-type-data.interface';
@@ -7,27 +7,29 @@ import { SelectEntityOfTypeResult } from '../types/select-entity-of-type-result.
 import { HYBRID_INJECTOR_HELPER } from '../../basics/step-basics.module';
 import { downgradeInjectable, getAngularJSGlobal } from '@angular/upgrade/static';
 import { AJS_MODULE } from '../../../shared';
-import { PlanDialogsService } from '../../../services/plan-dialogs.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EntityDialogsService {
   private _injector = inject(HYBRID_INJECTOR_HELPER);
   private _matDialog = inject(MatDialog);
 
-  selectEntityOfType(entityName: string, singleSelection: boolean, targetId?: string): Observable<SelectEntityOfTypeResult> {
+  selectEntityOfType(
+    entityName: string,
+    singleSelection: boolean,
+    targetId?: string
+  ): Observable<SelectEntityOfTypeResult> {
     return this._matDialog
       .open<SelectEntityOfTypeComponent, SelectEntityOfTypeData, SelectEntityOfTypeResult | undefined>(
         SelectEntityOfTypeComponent,
         {
-          data: {entityName, singleSelection, targetId},
-          injector: this._injector
-        },
+          data: { entityName, singleSelection, targetId },
+          injector: this._injector,
+        }
       )
-      .afterClosed().pipe(
-        filter((result) => !!result)
-      ) as Observable<SelectEntityOfTypeResult>;
+      .afterClosed()
+      .pipe(filter((result) => !!result)) as Observable<SelectEntityOfTypeResult>;
   }
 }
 
