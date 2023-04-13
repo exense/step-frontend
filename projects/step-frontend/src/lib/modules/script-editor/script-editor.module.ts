84a12d661d9e9761545ef5eb04b96395c7ba7296
Vladimir Adamov <vladimir@logicflow.ai>
2023-04-13 15:27:55 +0000

    SED-1544 Migrate code editor

--- projects/step-frontend/src/lib/modules/script-editor/script-editor.module.ts
+++ projects/step-frontend/src/lib/modules/script-editor/script-editor.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { StepCoreModule } from '@exense/step-core';
 import { StepCommonModule } from '../_common/step-common.module';
 import { ScriptEditorComponent } from './components/script-editor/script-editor.component';
+import './components/script-editor/script-editor.component';
 
 @NgModule({
   declarations: [ScriptEditorComponent],
