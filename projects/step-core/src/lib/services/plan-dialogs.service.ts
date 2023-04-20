c346fa1f0a15a4a95d20988ad7b8684563d382be
Vladimir <dragonvladir@gmail.com>
2023-04-20 12:17:18 +0300

    SED-1991 [OS] Migrate createPlanDialog
    * SED-1991 [OS] Migrate createPlanDialog
    
    * SED-1991 [OS] Migrate createPlanDialog (fix)
    
    * SED-1991 imports fix
    
    * SED-1991 [OS] Migrate createPlanDialog. Incorrect artefactTypes$ array

--- projects/step-core/src/lib/services/plan-dialogs.service.ts
+++ projects/step-core/src/lib/services/plan-dialogs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { catchError, map, Observable, of, switchMap, tap } from 'rxjs';
 import { AugmentedPlansService, Plan } from '../client/step-client-module';
 import { a1Promise2Observable, AJS_MODULE, DialogsService } from '../shared';
@@ -9,30 +9,25 @@ import { ResourceInputBridgeService } from './resource-input-bridge.service';
 import { UibModalHelperService } from './uib-modal-helper.service';
 import { EntityDialogsService } from '../modules/entity/services/entity-dialogs.service';
 import { downgradeInjectable, getAngularJSGlobal } from '@angular/upgrade/static';
+import { MatDialog } from '@angular/material/dialog';
+import { PlanCreateDialogComponent } from '../components/plan-create-dialog/plan-create-dialog.component';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlanDialogsService {
-  constructor(
-    private _plansApiService: AugmentedPlansService,
-    private _uibModalHelper: UibModalHelperService,
-    private _dialogs: DialogsService,
-    private _entityDialogs: EntityDialogsService,
-    private _exportDialogs: ExportDialogsService,
-    private _importDialogs: ImportDialogsService,
-    private _isUsedByDialogs: IsUsedByDialogService,
-    private _resourceInputBridgeService: ResourceInputBridgeService
-  ) {}
+  private _matDialog = inject(MatDialog);
+  private _plansApiService = inject(AugmentedPlansService);
+  private _uibModalHelper = inject(UibModalHelperService);
+  private _dialogs = inject(DialogsService);
+  private _entityDialogs = inject(EntityDialogsService);
+  private _exportDialogs = inject(ExportDialogsService);
+  private _importDialogs = inject(ImportDialogsService);
+  private _isUsedByDialogs = inject(IsUsedByDialogService);
+  private _resourceInputBridgeService = inject(ResourceInputBridgeService);
 
   createPlan(): Observable<any> {
-    const modalInstance = this._uibModalHelper.open({
-      backdrop: 'static',
-      templateUrl: 'partials/plans/createPlanDialog.html',
-      controller: 'createPlanCtrl',
-      resolve: {},
-    });
-    return a1Promise2Observable(modalInstance.result);
+    return this._matDialog.open(PlanCreateDialogComponent).afterClosed();
   }
 
   selectPlan(): Observable<Plan> {
