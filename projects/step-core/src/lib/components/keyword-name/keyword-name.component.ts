cf6a8fb39832f0005a574bec4ad3f0d222579a94
Vladimir <dragonvladir@gmail.com>
2023-04-20 18:11:10 +0300

    SED-1984 call plan selection attributes
    * SED-1984 Cannot update the CallPlan selection attributes
    
    * SED-1984 Build fix
    
    * SED-1984 Cannot update the CallPlan selection attributes (Remove ng-deep)
    
    * SED-1984 Cannot update the CallPlan selection attributes (Put back to linux style)

--- projects/step-core/src/lib/components/keyword-name/keyword-name.component.ts
+++ projects/step-core/src/lib/components/keyword-name/keyword-name.component.ts
@@ -1,35 +1,16 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  Optional,
-  Output,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, EventEmitter, forwardRef, inject, Input, Output } from '@angular/core';
 import { downgradeComponent, getAngularJSGlobal } from '@angular/upgrade/static';
 import {
   AbstractArtefact,
   AugmentedKeywordsService,
   CallFunction,
+  DynamicValueString,
   Function as Keyword,
 } from '../../client/step-client-module';
 import { AJS_MODULE } from '../../shared';
-import { EntityScopeResolver, Entity } from '../../modules/entity/entity.module';
-import { DynamicFieldsSchema, DynamicFieldGroupValue } from '../../modules/dynamic-forms/dynamic-forms.module';
-import { DynamicAttributePipe } from '../../pipes/dynamic-attribute.pipe';
-import { ArtefactRefreshNotificationService } from '../../services/artefact-refresh-notification.service';
-import { Subject, takeUntil } from 'rxjs';
-
-interface KeywordMeta {
-  icon: string;
-  tooltip?: string;
-  description: string;
-  isError?: boolean;
-}
+import { DynamicFieldsSchema } from '../../modules/dynamic-forms/dynamic-forms.module';
+import { ReferenceArtefactNameConfig } from '../reference-artefact-name/reference-artefact-name.component';
+import { Observable } from 'rxjs';
 
 const KEYWORD_ATTRIBUTES_SCHEMA: DynamicFieldsSchema = {
   properties: {
@@ -38,13 +19,31 @@ const KEYWORD_ATTRIBUTES_SCHEMA: DynamicFieldsSchema = {
   required: ['name'],
 };
 
+const KEYWORD_CAPTIONS: ReferenceArtefactNameConfig<CallFunction, Keyword>['captions'] = {
+  searchReference: 'Select a keyword',
+  dynamicReference: 'Dynamic keyword',
+  referenceNotFound: 'Keyword not found',
+  referenceLabel: 'Reference keyword',
+  editSelectionCriteria: 'Edit Keyword selection criteria',
+  selectionCriteria: 'Keyword selection criteria',
+  selectionCriteriaDescription:
+    'The Keyword selection criteria are used to reference the Keyword. Besides the name any other attribute of the Keyword can be referenced.',
+  addSelectionCriteriaLabel: 'Add selection criteria',
+};
+
 @Component({
   selector: 'step-keyword-name',
   templateUrl: './keyword-name.component.html',
   styleUrls: ['./keyword-name.component.scss'],
+  providers: [
+    {
+      provide: ReferenceArtefactNameConfig,
+      useExisting: forwardRef(() => KeywordNameComponent),
+    },
+  ],
 })
-export class KeywordNameComponent implements OnChanges, OnInit, OnDestroy {
-  private terminator$ = new Subject<void>();
+export class KeywordNameComponent implements ReferenceArtefactNameConfig<CallFunction, Keyword> {
+  private _keywordApi = inject(AugmentedKeywordsService);
 
   @Input() isDisabled: boolean = false;
   @Input() artefact?: CallFunction;
@@ -52,193 +51,19 @@ export class KeywordNameComponent implements OnChanges, OnInit, OnDestroy {
   @Output() onSave = new EventEmitter<unknown>();
 
   @Output() keywordUpdate = new EventEmitter<Keyword | undefined>();
-  readonly KEYWORD_ATTRIBUTES_SCHEMA = KEYWORD_ATTRIBUTES_SCHEMA;
-  protected artefactName: string = '';
-  protected artefactKeywordAttributes?: DynamicFieldGroupValue;
-  protected referenceKeywordString: string = '';
-  protected keyword?: Keyword;
-  protected isEditorMode: boolean = false;
-  protected keywordMeta?: KeywordMeta;
 
-  constructor(
-    private _keywordApi: AugmentedKeywordsService,
-    private _entityScopeResolver: EntityScopeResolver,
-    private _changeDetectorRef: ChangeDetectorRef,
-    @Optional() private _artefactRefreshNotification: ArtefactRefreshNotificationService
-  ) {}
+  readonly artefactClass = 'CallKeyword';
 
-  ngOnInit(): void {
-    this.setupArtefactExternalRefresh();
+  readonly referenceAttributesSchema = KEYWORD_ATTRIBUTES_SCHEMA;
+
+  readonly captions = KEYWORD_CAPTIONS;
+
+  getSearchAttributes(artefact: CallFunction): DynamicValueString | undefined {
+    return artefact!.function;
   }
 
-  ngOnDestroy(): void {
-    this.terminator$.next();
-    this.terminator$.complete();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const cArtefact = changes['artefact'];
-    if (cArtefact?.previousValue !== cArtefact?.currentValue || cArtefact.firstChange) {
-      this.initArtefactName(cArtefact?.currentValue);
-      this.parseArtefactKeywordAttributes(cArtefact?.currentValue);
-      this.loadArtefactKeyword(cArtefact?.currentValue);
-    }
-  }
-
-  onArtefactNameChange(newName: string): void {
-    this.artefactName = newName;
-    if (!this.artefact) {
-      return;
-    }
-    if (!this.artefact.attributes) {
-      this.artefact.attributes = {};
-    }
-    this.artefact.attributes['name'] = newName;
-    this.onSave.emit();
-  }
-
-  updateKeywordAttributes(attributes?: DynamicFieldGroupValue): void {
-    if (!this.artefact) {
-      return;
-    }
-
-    this.artefactKeywordAttributes = attributes;
-    const attributesString = JSON.stringify(attributes);
-    const reloadKeyword = this.artefact!.function!.value !== attributesString;
-    if (reloadKeyword) {
-      this.artefact!.function!.value = attributesString;
-      this.loadArtefactKeyword(this.artefact);
-    }
-    this.createReferenceKeywordString(this.artefactKeywordAttributes);
-
-    this.onSave.emit();
-  }
-
-  private initArtefactName(artefact?: CallFunction): void {
-    this.artefactName = '';
-    this._changeDetectorRef.detectChanges();
-    this.artefactName = artefact?.attributes?.['name'] || '';
-  }
-
-  private parseArtefactKeywordAttributes(artefact?: any): void {
-    if (!artefact) {
-      this.artefactKeywordAttributes = undefined;
-      this.createReferenceKeywordString(this.artefactKeywordAttributes);
-      return;
-    }
-
-    try {
-      let attributesJson = artefact?.function?.value;
-
-      this.artefactKeywordAttributes = attributesJson ? JSON.parse(attributesJson) : undefined;
-      if (typeof this.artefactKeywordAttributes?.['name'] === 'string') {
-        this.artefactKeywordAttributes['name'] = {
-          value: this.artefactKeywordAttributes?.['name'],
-          dynamic: !!artefact?.function?.dynamic,
-          expression: artefact?.function?.expression || '',
-        };
-      }
-    } catch (err) {
-      this.artefactKeywordAttributes = undefined;
-    }
-    this.createReferenceKeywordString(this.artefactKeywordAttributes);
-  }
-
-  private createReferenceKeywordString(attributes?: DynamicFieldGroupValue): void {
-    if (!attributes) {
-      this.referenceKeywordString = '';
-      return;
-    }
-
-    const nameValue = DynamicAttributePipe.transform(attributes['name']) || '';
-    const namePart = nameValue ? `name=${nameValue}` : '';
-
-    const otherParts = Object.entries(attributes)
-      .filter(([key]) => key !== 'name')
-      .map(([key, attribute]) => `${key}=${DynamicAttributePipe.transform(attribute) || ''}`);
-
-    this.referenceKeywordString =
-      otherParts.length === 0 ? nameValue.toString() : [namePart, ...otherParts].filter((part) => !!part).join(', ');
-  }
-
-  private hasDynamicParameters(): boolean {
-    return Object.values(this.artefactKeywordAttributes || {}).some((attribute) => attribute.dynamic);
-  }
-
-  private isEmptyKeywordName(): boolean {
-    const nameAttribute = (this.artefactKeywordAttributes || {})['name'];
-    if (!nameAttribute) {
-      return true;
-    }
-    return nameAttribute.dynamic ? !nameAttribute.expression : !nameAttribute.value;
-  }
-
-  private loadArtefactKeyword(artefact?: CallFunction): void {
-    if (!artefact) {
-      this.keyword = undefined;
-      this.keywordUpdate.emit(undefined);
-      this.keywordMeta = undefined;
-      return;
-    }
-
-    if ((artefact as AbstractArtefact)._class !== 'CallKeyword') {
-      // For some reason and angularJS template, the template doesn't switch in time.
-      // It creates the situation when non keyword artefact passed to this component.
-      // In that case lookup with throw exception on the backend.
-      // To avoid it, this if statement was added.
-      // It could be removed when whole editor's form will be migrated
-      return;
-    }
-
-    if (this.isEmptyKeywordName()) {
-      this.keyword = undefined;
-      this.keywordUpdate.emit(undefined);
-      this.keywordMeta = {
-        icon: 'search',
-        description: 'Select a keyword',
-      };
-      if (!this.isDisabled && !this.isEditorMode) {
-        this.isEditorMode = true;
-      }
-      return;
-    }
-
-    this._keywordApi.lookupCallFunction(artefact).subscribe((keyword) => {
-      this.keyword = keyword;
-      this.keywordUpdate.emit(keyword);
-      if (!keyword) {
-        if (this.hasDynamicParameters()) {
-          this.keywordMeta = {
-            icon: 'zap',
-            description: 'Dynamic keyword',
-          };
-        } else {
-          this.keywordMeta = {
-            icon: 'alert-circle',
-            description: 'Keyword not found',
-            isError: true,
-          };
-        }
-        return;
-      }
-      const entityScope = this._entityScopeResolver.getScope(keyword as Entity);
-      this.keywordMeta = entityScope
-        ? {
-            icon: entityScope.icon,
-            tooltip: entityScope.tooltip,
-            description: entityScope.tenant,
-          }
-        : undefined;
-    });
-  }
-
-  private setupArtefactExternalRefresh(): void {
-    if (!this._artefactRefreshNotification) {
-      return;
-    }
-    this._artefactRefreshNotification.refreshArtefact$
-      .pipe(takeUntil(this.terminator$))
-      .subscribe(() => this.initArtefactName(this.artefact));
+  lookupReference(artefact: AbstractArtefact): Observable<Keyword> {
+    return this._keywordApi.lookupCallFunction(artefact);
   }
 }
 
