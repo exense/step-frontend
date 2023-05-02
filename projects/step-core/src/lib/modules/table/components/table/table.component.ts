269ff8ae24df824172b48c686781b800b818530f
bojidaryovchev <bozhidar.yovchev@exense.ch>
2023-05-02 11:49:30 +0000

    SED-1986 When scrolling tables global actions should stay fixed

--- projects/step-core/src/lib/modules/table/components/table/table.component.ts
+++ projects/step-core/src/lib/modules/table/components/table/table.component.ts
@@ -14,6 +14,7 @@ import {
   TemplateRef,
   TrackByFunction,
   ViewChild,
+  ViewEncapsulation,
 } from '@angular/core';
 import { BehaviorSubject, combineLatest, map, Observable, of, startWith, Subject, takeUntil } from 'rxjs';
 import { TableDataSource } from '../../shared/table-data-source';
@@ -47,6 +48,7 @@ export type DataSource<T> = TableDataSource<T> | T[] | Observable<T[]>;
   selector: 'step-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
+  encapsulation: ViewEncapsulation.None,
   providers: [
     {
       provide: TableSearch,
