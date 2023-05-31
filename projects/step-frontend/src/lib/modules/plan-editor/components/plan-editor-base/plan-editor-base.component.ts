6f895f39b3af33419aa683fbf1cc87f2774083c2
Tim Rasim <neogucky@gmail.com>
2023-05-31 15:15:17 +0200

    SED-2051 moved logic to composite specific place

--- projects/step-frontend/src/lib/modules/plan-editor/components/plan-editor-base/plan-editor-base.component.ts
+++ projects/step-frontend/src/lib/modules/plan-editor/components/plan-editor-base/plan-editor-base.component.ts
@@ -113,7 +113,6 @@ export class PlanEditorBaseComponent
   protected repositoryObjectRef?: RepositoryObjectReference;
 
   protected planClass?: string;
-  protected customFields?: Record<string, any>;
   private terminator$ = new Subject<void>();
   @ViewChild('keywordCalls', { read: KeywordCallsComponent, static: false })
   private keywords?: KeywordCallsComponent;
@@ -294,7 +293,6 @@ export class PlanEditorBaseComponent
       )
       .subscribe((plan) => {
         this.planClass = plan._class;
-        this.customFields = plan.customFields;
         this.planTypeControl.setValue(
           {
             planType: plan.root!._class,
