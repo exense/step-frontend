6f2656e808d8b975b333bbdbf3944ab351999364
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2023-06-14 14:58:42 +0200

    SED-2176 migrated plan selection table

--- projects/step-core/src/lib/client/augmented/services/augmented-plans.service.ts
+++ projects/step-core/src/lib/client/augmented/services/augmented-plans.service.ts
@@ -1,16 +1,23 @@
 import { inject, Injectable } from '@angular/core';
-import { Plan, PlansService } from '../../generated';
+import { Parameter, Plan, PlansService } from '../../generated';
 import { StepDataSource, TableRemoteDataSourceFactoryService } from '../../table/step-table-client.module';
 
 @Injectable({ providedIn: 'root' })
 export class AugmentedPlansService extends PlansService {
+  private readonly PLANS_TABLE_ID = 'plans';
   private _dataSourceFactory = inject(TableRemoteDataSourceFactoryService);
 
   getPlansTableDataSource(): StepDataSource<Plan> {
-    return this._dataSourceFactory.createDataSource('plans', {
+    return this._dataSourceFactory.createDataSource(this.PLANS_TABLE_ID, {
       name: 'attributes.name',
       type: 'root._class',
       actions: '',
+    });
+  }
+
+  createSelectionDataSource(): StepDataSource<Plan> {
+    return this._dataSourceFactory.createDataSource(this.PLANS_TABLE_ID, {
+      scope: 'scope',
     });
   }
 }
