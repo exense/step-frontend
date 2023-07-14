a279df80773dee80d2c0377c7737161b4c341580
dvladir <dragonvladir@gmail.com>
2023-07-14 12:21:54 +0300

    SED-2303 Status filter missing in scheduler view

---
+++ projects/step-core/src/lib/modules/table/shared/boolean-filter-condition.ts
@@ -0,0 +1,34 @@
+import { FilterCondition } from './filter-condition';
+import { FilterConditionType } from './filter-condition-type.enum';
+import { TableRequestFilter } from '../../../client/table/shared/table-request-data';
+import { TableCollectionFilter } from '../../../client/table/shared/table-collection-filter';
+import { CompareCondition } from '../../basics/shared/compare-condition.enum';
+
+export class BooleanFilterCondition extends FilterCondition<string> {
+  readonly filterConditionType = FilterConditionType.BOOLEAN;
+
+  constructor(searchValue?: string) {
+    super(searchValue);
+  }
+
+  override isEmpty(): boolean {
+    return !this.sourceObject;
+  }
+
+  override toRequestFilter(field: string): Array<TableRequestFilter | undefined> {
+    const value = (this.sourceObject || '').toLowerCase();
+    if (value !== true.toString() && value !== false.toString()) {
+      return [];
+    }
+    const expectedValue = value === true.toString();
+
+    const boolFilter: TableCollectionFilter = {
+      collectionFilter: {
+        type: CompareCondition.EQUALS,
+        field,
+        expectedValue,
+      },
+    };
+    return [boolFilter];
+  }
+}
