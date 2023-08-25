f307b12fd486d48a6d8b7d7b3e5613af8e362c97
Vladimir <dragonvladir@gmail.com>
2023-08-25 12:35:51 +0300

    SED-2324 Keyword output with nested json not properly displayed on "Keyword calls" table (#540)
    * SED-2324 Keyword output with nested json not properly displayed on "Keyword calls" table
    
    * SED-2324 Migrate json viewers
    
    * SED-2324 removed broken imports

---
+++ projects/step-core/src/lib/modules/json-viewer/components/pretty-print-inline/pretty-print-inline.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { BaseViewerComponent } from '../base-viewer/base-viewer.component';
+import { JsonViewerFormatterService } from '../../services/json-viewer-formatter.service';
+
+@Component({
+  selector: 'step-pretty-print-inline',
+  templateUrl: './pretty-print-inline.component.html',
+  styleUrls: ['./pretty-print-inline.component.scss'],
+})
+export class PrettyPrintInlineComponent extends BaseViewerComponent {
+  private _jsonViewerFormatter = inject(JsonViewerFormatterService);
+
+  protected formatValue(json: unknown, maxChars?: number): string {
+    const jsonString = this._jsonViewerFormatter.formatToJsonString(json);
+    if (maxChars && maxChars < jsonString.length) {
+      return `${jsonString.substring(0, maxChars)}...`;
+    } else {
+      return jsonString.substring(0, jsonString.length - 2);
+    }
+  }
+}
