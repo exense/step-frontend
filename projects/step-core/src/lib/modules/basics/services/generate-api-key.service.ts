d8ad2afedb4858e152d99fe118f0c45af6d9cc53
Tim Rasim <neogucky@gmail.com>
2023-08-30 15:33:22 +0200

    SED-2373 Listing all API keys and giving possibility to revoke (#556)
    * SED-2373 Listing all API keys and giving possibility to revoke
    
    * SED-2373 Added interface for API-key
    
    * SED-2373 fixed type issue

--- projects/step-core/src/lib/modules/basics/services/generate-api-key.service.ts
+++ projects/step-core/src/lib/modules/basics/services/generate-api-key.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { GenerateApiKeyStrategy } from '../shared/generate-api-key-strategy';
+import { Observable } from 'rxjs';
+import { ApiToken } from '../shared/api-token.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +13,24 @@ export class GenerateApiKeyService implements GenerateApiKeyStrategy {
     this.strategy = strategy;
   }
 
-  showGenerateApiKeyDialog(): void {
+  showGenerateApiKeyDialog(): Observable<ApiToken> {
     if (!this.strategy) {
-      return;
+      console.error('GenerateApiKeyService', 'Missing ServiceAccount strategy');
     }
-    this.strategy.showGenerateApiKeyDialog();
+    return this.strategy!.showGenerateApiKeyDialog();
+  }
+
+  getServiceAccountTokens(): Observable<Array<ApiToken>> {
+    if (!this.strategy) {
+      console.error('GenerateApiKeyService', 'Missing ServiceAccount strategy');
+    }
+    return this.strategy!.getServiceAccountTokens();
+  }
+
+  revoke(id: string): Observable<any> {
+    if (!this.strategy) {
+      console.error('GenerateApiKeyService', 'Missing ServiceAccount strategy');
+    }
+    return this.strategy!.revoke(id);
   }
 }
