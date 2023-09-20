b220ad594c4ebaa71fc3ae3ff9fedff5991a7591
dvladir <dragonvladir@gmail.com>
2023-09-20 16:05:01 +0300

    SED-2388 FE: Add material to be able to use Docker container

--- projects/step-core/src/lib/components/base-artefact/base-artefact.component.ts
+++ projects/step-core/src/lib/components/base-artefact/base-artefact.component.ts
@@ -1,9 +1,17 @@
 import { AfterViewInit, Component, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AbstractArtefact } from '../../client/step-client-module';
+import {
+  AbstractArtefact,
+  DynamicValueBoolean,
+  DynamicValueInteger,
+  DynamicValueString,
+} from '../../client/step-client-module';
 import { CustomComponent } from '../../modules/custom-registeries/custom-registries.module';
 import { ArtefactFormChangeHelperService } from '../../services/artefact-form-change-helper.service';
 import { ArtefactContext } from '../../shared';
+
+type SimpleValue = undefined | null | string | boolean | number;
+type PossibleField = SimpleValue | DynamicValueString | DynamicValueInteger | DynamicValueBoolean;
 
 @Component({
   template: '',
@@ -23,5 +31,26 @@ export abstract class BaseArtefactComponent<T extends AbstractArtefact> implemen
 
   ngAfterViewInit(): void {
     this._artefactFormChangeHelper.setupFormBehavior(this.form, () => this.context.save());
+  }
+
+  protected determineEmptyGroup(fields: PossibleField[], emptyValues: SimpleValue[] = [undefined, null, '', 0, false]) {
+    const isDynamic = (field: PossibleField) =>
+      !!field && field.hasOwnProperty('dynamic') && field.hasOwnProperty('value') && field.hasOwnProperty('expression');
+
+    const extractValue = (field: PossibleField): SimpleValue => {
+      if (!isDynamic(field)) {
+        return field as SimpleValue;
+      }
+      const dynamicField = field as Omit<DynamicValueString, 'value'> & { value: SimpleValue };
+      return dynamicField.dynamic ? dynamicField.expression : dynamicField.value;
+    };
+
+    for (const field of fields) {
+      const fieldValue = extractValue(field);
+      if (!emptyValues.includes(fieldValue)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
