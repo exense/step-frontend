3d38cd9ffff85d920f62483ea9a271b9f21a5237
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2023-09-28 14:12:24 +0200

    SED-2449 Migrated UserSelection table

---
+++ projects/step-frontend/src/lib/modules/admin/components/user-selection/user-selection.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, ViewChild } from '@angular/core';
+import {
+  AugmentedUserService,
+  AutoDeselectStrategy,
+  BaseEntitySelectionTableComponent,
+  User,
+  selectionCollectionProvider,
+  SelectionCollector,
+  TableComponent,
+} from '@exense/step-core';
+
+@Component({
+  selector: 'step-user-selection',
+  templateUrl: './user-selection.component.html',
+  styleUrls: ['./user-selection.component.scss'],
+  providers: [selectionCollectionProvider<string, User>('id', AutoDeselectStrategy.DESELECT_ON_UNREGISTER)],
+})
+export class UserSelectionComponent extends BaseEntitySelectionTableComponent {
+  protected _selectionCollector = inject<SelectionCollector<string, User>>(SelectionCollector);
+  @ViewChild('tableRef', { read: TableComponent })
+  protected _tableRef?: TableComponent<User>;
+  readonly _dataSource = inject(AugmentedUserService).createSelectionDataSource();
+}
