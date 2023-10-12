9d3690cc04e15c1f52713552b9ded2e5a8d4cf5b
dvladir <dragonvladir@gmail.com>
2023-10-12 12:41:31 +0300

    SED-2482-migrate-admin-settings

--- projects/step-core/src/lib/components/settings/settings.component.ts
+++ projects/step-core/src/lib/components/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, TrackByFunction } from '@angular/core';
-import { SubRouteData, ViewRegistryService } from '@exense/step-core';
 import { ActivatedRoute } from '@angular/router';
+import { ViewRegistryService } from '../../services/view-registry.service';
+import { SubRouteData } from '../../shared';
 
 @Component({
   selector: 'step-settings',
