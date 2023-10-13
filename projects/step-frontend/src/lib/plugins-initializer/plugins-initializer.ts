25c1d8388e4165b10e39e8cb35cecb857510162e
build server <dev@exense.ch>
2023-10-13 10:26:12 +0200

    Building 3.23.0

--- projects/step-frontend/src/lib/plugins-initializer/plugins-initializer.ts
+++ projects/step-frontend/src/lib/plugins-initializer/plugins-initializer.ts
@@ -56,7 +56,7 @@ const fetchDefinitions = async (): Promise<PluginDefinition[]> => {
       //@ts-ignore
       if (plugin['entryPoint']) {
         //@ts-ignore
-        plugin['entryPoint'] += '?v=${project.version}';
+        plugin['entryPoint'] += '?v=3.23.0';
       }
 
       const angularModules = (plugin as LegacyPluginDefinition)?.angularModules || [];
