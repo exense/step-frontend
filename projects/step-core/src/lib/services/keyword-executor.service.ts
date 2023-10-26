7d6e842b7d90c68cc954dcdbb40740daa70500d7
Vladimir <dragonvladir@gmail.com>
2023-10-27 00:36:51 +0300

    SED-1667 Align callKeyword form pre-population between plan editor and keyword execution (#595)
    * SED-1667 Align callKeyword form pre-population between plan editor and keyword execution
    
    * SED-2219 Align KW naming in augmented services (#597)
    
    * SED-2221 Rename enum property keys to be SCREAMING_SNAKE_CASE (#594)
    
    * SED-2310 update step icons (#579)
    
    * SED-2310 updated plan type icons
    
    * SED-2310 updated important icons
    
    * SED-2310 solved merge conflicts
    
    * SED-2310 Small UX improvement
    
    * SED-2310 updated entity resolver mechanism
    
    * SED-2310 updated icon behaviour
    
    * SED-2310 regenerated icons
    
    * SED-2310 fixed missing bulk operators
    
    * SED-1667 Solve conflicts

---
+++ projects/step-core/src/lib/services/keyword-executor.service.ts
@@ -0,0 +1,42 @@
+import { inject, Injectable } from '@angular/core';
+import { forkJoin, map, switchMap, tap } from 'rxjs';
+import { ArtefactsFactoryService } from './artefacts-factory.service';
+import { PlanOpenService } from './plan-open.service';
+import { AugmentedPlansService } from '../client/step-client-module';
+
+const PLAN_TYPE = 'step.core.plans.Plan';
+const PLAN_TEMPLATE = 'Session';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class KeywordExecutorService {
+  private _planApi = inject(AugmentedPlansService);
+  private _artefactsFactory = inject(ArtefactsFactoryService);
+  private _planOpen = inject(PlanOpenService);
+
+  executeKeyword(keywordId: string): void {
+    const plan$ = this._planApi.newPlan(PLAN_TYPE, PLAN_TEMPLATE).pipe(
+      tap((createdPlan) => {
+        createdPlan!.attributes = createdPlan!.attributes ?? {};
+        createdPlan!.attributes!['name'] = PLAN_TEMPLATE;
+        createdPlan!.visible = false;
+      })
+    );
+
+    const keywordArtefact$ = this._artefactsFactory.createCallKeywordArtefact(keywordId);
+
+    forkJoin([plan$, keywordArtefact$])
+      .pipe(
+        map(([plan, keywordArtefact]) => {
+          plan!.root!.children = [keywordArtefact];
+          return plan;
+        }),
+        switchMap((plan) => this._planApi.savePlan(plan))
+      )
+      .subscribe((plan) => {
+        const artefactId = plan!.root!.children![0]!.id;
+        this._planOpen.open(plan.id!, { artefactId, startInteractive: true });
+      });
+  }
+}
