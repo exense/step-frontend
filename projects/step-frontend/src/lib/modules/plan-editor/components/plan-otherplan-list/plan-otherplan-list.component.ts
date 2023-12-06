f9000fbc0baec56fcf9d84aea73e7a2e93eca6e0
dvladir <dragonvladir@gmail.com>
2023-12-06 16:32:16 +0300

    SED-2525 Add global filter on plan editor components tables

--- projects/step-frontend/src/lib/modules/plan-editor/components/plan-otherplan-list/plan-otherplan-list.component.ts
+++ projects/step-frontend/src/lib/modules/plan-editor/components/plan-otherplan-list/plan-otherplan-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output, ViewEncapsulation } from '@angular/core';
 import { AugmentedPlansService } from '@exense/step-core';
 
 @Component({
   selector: 'step-plan-otherplan-list',
   templateUrl: './plan-otherplan-list.component.html',
   styleUrls: ['./plan-otherplan-list.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class PlanOtherplanListComponent {
   @Output() onSelection = new EventEmitter<string>();
