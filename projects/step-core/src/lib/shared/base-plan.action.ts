e8d15e124c67b0083e5e746ee2a28fbba310d42f
dvladir <dragonvladir@gmail.com>
2023-12-15 15:08:03 +0300

    SED-1896 build fixes

--- projects/step-core/src/lib/shared/base-plan.action.ts
+++ projects/step-core/src/lib/shared/base-plan.action.ts
@@ -1,4 +1,4 @@
-import { EntityActionInterceptor } from '../modules/entity/entity.module';
+import { EntityActionInterceptor } from '../modules/entity/injectables/entity-action.interceptor';
 import { Plan } from '../client/step-client-module';
 
 export abstract class BasePlanAction extends EntityActionInterceptor<Plan> {
--- projects/step-core/src/lib/shared/plugin-lazy-load.ts
+++ projects/step-core/src/lib/shared/plugin-lazy-load.ts
@@ -1,10 +1,11 @@
 import { PluginOnInit } from './plugin-on-init';
-import { ApplicationRef, Compiler, createNgModule, inject, Injector, ModuleWithProviders, Type } from '@angular/core';
+import { Compiler, inject, Injector, Type } from '@angular/core';
 import { PluginInfoRegistryService } from '../services/plugin-info-registry.service';
 import { IModule } from 'angular';
 
 export interface LazyLoadPluginMeta {
   Module: Type<any>;
+  ajsModuleName?: string;
 }
 
 export type ImportMeta =
@@ -15,8 +16,11 @@ export type ImportMeta =
     };
 
 export abstract class PluginLazyLoad implements PluginOnInit {
+  private _compiler = inject(Compiler);
   private _injector = inject(Injector);
   private _pluginInfoRegistry = inject(PluginInfoRegistryService);
+
+  constructor(private parentAjsModule?: IModule) {}
 
   pluginOnInit(): Promise<unknown> {
     const metaDictionary = this.getPluginsLazyLoadMeta();
@@ -38,8 +42,12 @@ export abstract class PluginLazyLoad implements PluginOnInit {
     console.log(`PLUGIN "${pluginName}" LAZYLOAD`);
     return load()
       .then((meta) => {
-        return createNgModule(meta.Module, this._injector);
+        if (meta.ajsModuleName && this.parentAjsModule) {
+          this.parentAjsModule.requires.push(meta.ajsModuleName);
+        }
+        return this._compiler.compileModuleAsync(meta.Module);
       })
+      .then((moduleFactory) => moduleFactory.create(this._injector))
       .then(() => console.log(`MODULE "${pluginName}" REGISTERED`));
   }
 }
