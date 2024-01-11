e9347930c949b9e8e165df89ce64056527f6e263
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2024-01-11 15:55:44 +0100

    SED-2715 changed behaviour of function click

--- projects/step-core/src/lib/modules/keywords-common/components/function-link/function-link.component.ts
+++ projects/step-core/src/lib/modules/keywords-common/components/function-link/function-link.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Injector, Input, Output } from '@angular/core';
 import { map, of } from 'rxjs';
 import { Keyword } from '../../../../client/step-client-module';
-import { CustomComponent } from '../../../custom-registeries/custom-registries.module';
-import { CustomColumnOptions } from '../../../table/table.module';
+import { CustomComponent } from '../../../custom-registeries/shared/custom-component';
+import { CustomColumnOptions, TableReload } from '../../../table/table.module';
 import { FunctionActionsService } from '../../injectables/function-actions.service';
 
 @Component({
@@ -13,19 +13,39 @@ import { FunctionActionsService } from '../../injectables/function-actions.servi
 export class FunctionLinkComponent implements CustomComponent {
   private _functionActions = inject(FunctionActionsService, { optional: true });
   private _customColumnOptions = inject(CustomColumnOptions, { optional: true });
+  private _injector = inject(Injector);
+  private _tableReload = inject(TableReload, { optional: true });
 
   @Input() context?: Keyword;
+  @Input() openEditorOnClick?: boolean;
   @Output() edit = new EventEmitter<void>();
 
   readonly noLink$ = (this._customColumnOptions?.options$ || of([])).pipe(
     map((options) => options.includes('noEditorLink'))
   );
 
-  editFunction(): void {
+  handleClick(): void {
     if (!this.context?.id || !this._functionActions) {
       return;
     }
-    this._functionActions.openFunctionEditor(this.context).subscribe((continueEdit) => {
+
+    if (this.openEditorOnClick) {
+      this.openEditor(this.context);
+    } else {
+      this.editFunction(this.context);
+    }
+  }
+
+  editFunction(keyword: Keyword): void {
+    this._functionActions!.configureFunction(this._injector, keyword.id!).subscribe((keyword) => {
+      if (keyword && this._tableReload) {
+        this._tableReload.reload();
+      }
+    });
+  }
+
+  openEditor(keyword: Keyword): void {
+    this._functionActions!.openFunctionEditor(keyword).subscribe((continueEdit) => {
       if (continueEdit) {
         this.edit.emit();
       }
