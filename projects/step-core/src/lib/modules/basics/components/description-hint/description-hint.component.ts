4a20696c9694431eb59b17e9d573ad4d7abf1a44
dvladir <dragonvladir@gmail.com>
2024-01-12 16:49:33 +0300

    SED-2535 Improve tooltips in step

---
+++ projects/step-core/src/lib/modules/basics/components/description-hint/description-hint.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { MatMenu } from '@angular/material/menu';
+
+@Component({
+  selector: 'step-description-hint',
+  templateUrl: './description-hint.component.html',
+  styleUrls: ['./description-hint.component.scss'],
+})
+export class DescriptionHintComponent {
+  toggleActive = false;
+  @Input() description?: string;
+  @Input() xPosition: MatMenu['xPosition'] = 'after';
+  @Input() yPosition: MatMenu['yPosition'] = 'above';
+}
