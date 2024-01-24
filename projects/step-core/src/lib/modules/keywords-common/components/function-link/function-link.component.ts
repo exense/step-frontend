d14184f0175c61a430bf7d7d896c91723a55a8cc
Vladimir <dragonvladir@gmail.com>
2024-01-24 13:08:26 +0300

    SED-2774 Custom screen input for KW name to open the script editor (#698)

--- projects/step-core/src/lib/modules/keywords-common/components/function-link/function-link.component.ts
+++ projects/step-core/src/lib/modules/keywords-common/components/function-link/function-link.component.ts
@@ -1,54 +1,29 @@
-import { Component, EventEmitter, inject, Injector, Input, Output } from '@angular/core';
-import { map, of } from 'rxjs';
+import { Component, inject, Injector } from '@angular/core';
 import { Keyword } from '../../../../client/step-client-module';
 import { CustomComponent } from '../../../custom-registeries/shared/custom-component';
-import { CustomColumnOptions, TableReload } from '../../../table/table.module';
-import { FunctionActionsService } from '../../injectables/function-actions.service';
+import { TableReload } from '../../../table/table.module';
+import { BaseFunctionLinkComponent } from './base-function-link.component';
 
 @Component({
   selector: 'step-function-link',
   templateUrl: './function-link.component.html',
   styleUrls: ['./function-link.component.scss'],
 })
-export class FunctionLinkComponent implements CustomComponent {
-  private _functionActions = inject(FunctionActionsService, { optional: true });
-  private _customColumnOptions = inject(CustomColumnOptions, { optional: true });
+export class FunctionLinkComponent extends BaseFunctionLinkComponent implements CustomComponent {
   private _injector = inject(Injector);
   private _tableReload = inject(TableReload, { optional: true });
-  private readonly options$ = this._customColumnOptions?.options$ ?? of([]);
 
-  @Input() context?: Keyword;
-  @Input() openEditorOnClick?: boolean;
-  @Output() edit = new EventEmitter<void>();
-
-  readonly noLink$ = this.options$.pipe(map((options) => options.includes('noEditorLink')));
-
-  readonly noDescriptionHint$ = this.options$.pipe(map((options) => options.includes('noDescriptionHint')));
-
-  handleClick(): void {
+  override handleLinkClick(): void {
     if (!this.context?.id || !this._functionActions) {
       return;
     }
-
-    if (this.openEditorOnClick) {
-      this.openEditor(this.context);
-    } else {
-      this.editFunction(this.context);
-    }
+    this.editFunction(this.context);
   }
 
   editFunction(keyword: Keyword): void {
     this._functionActions!.configureFunction(this._injector, keyword.id!).subscribe((keyword) => {
       if (keyword && this._tableReload) {
         this._tableReload.reload();
-      }
-    });
-  }
-
-  openEditor(keyword: Keyword): void {
-    this._functionActions!.openFunctionEditor(keyword).subscribe((continueEdit) => {
-      if (continueEdit) {
-        this.edit.emit();
       }
     });
   }
