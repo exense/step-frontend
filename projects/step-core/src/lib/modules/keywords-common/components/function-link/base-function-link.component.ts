176daf6346d528090e88e76e64295fd943e8b079
Vladimir <dragonvladir@gmail.com>
2024-01-24 13:08:26 +0300

    SED-2774 Custom screen input for KW name to open the script editor (#698)

---
+++ projects/step-core/src/lib/modules/keywords-common/components/function-link/base-function-link.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, Input } from '@angular/core';
+import { map, of } from 'rxjs';
+import { CustomComponent } from '../../../custom-registeries/custom-registries.module';
+import { CustomColumnOptions } from '../../../table/table.module';
+import { Keyword } from '../../../../client/step-client-module';
+import { FunctionActionsService } from '../../injectables/function-actions.service';
+
+@Component({
+  template: '',
+})
+export abstract class BaseFunctionLinkComponent implements CustomComponent {
+  protected _functionActions = inject(FunctionActionsService, { optional: true });
+  private _customColumnOptions = inject(CustomColumnOptions, { optional: true });
+  private readonly options$ = this._customColumnOptions?.options$ ?? of([]);
+
+  @Input() context?: Keyword;
+
+  readonly noLink$ = this.options$.pipe(map((options) => options.includes('noEditorLink')));
+
+  readonly noDescriptionHint$ = this.options$.pipe(map((options) => options.includes('noDescriptionHint')));
+
+  abstract handleLinkClick(): void;
+}
---
+++ projects/step-core/src/lib/modules/keywords-common/components/function-link/function-link-editor.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { BaseFunctionLinkComponent } from './base-function-link.component';
+import { CustomComponent } from '../../../custom-registeries/custom-registries.module';
+import { Keyword } from '../../../../client/step-client-module';
+
+@Component({
+  selector: 'step-function-link-editor',
+  templateUrl: './function-link.component.html',
+  styleUrls: ['./function-link.component.scss'],
+})
+export class FunctionLinkEditorComponent extends BaseFunctionLinkComponent implements CustomComponent {
+  @Output() edit = new EventEmitter<void>();
+
+  override handleLinkClick(): void {
+    if (!this.context?.id || !this._functionActions) {
+      return;
+    }
+    this.openEditor(this.context);
+  }
+
+  private openEditor(keyword: Keyword): void {
+    this._functionActions!.openFunctionEditor(keyword).subscribe((continueEdit) => {
+      if (continueEdit) {
+        this.edit.emit();
+      }
+    });
+  }
+}
