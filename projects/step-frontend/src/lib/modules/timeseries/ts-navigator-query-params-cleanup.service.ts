669afefd0ece1b3112dd989db513941470931d89
dvladir <dragonvladir@gmail.com>
2024-02-13 13:31:26 +0300

    SED-2615 expose table filters as query parameters. Fix after merge

--- projects/step-frontend/src/lib/modules/timeseries/ts-navigator-query-params-cleanup.service.ts
+++ projects/step-frontend/src/lib/modules/timeseries/ts-navigator-query-params-cleanup.service.ts
@@ -1,17 +1,17 @@
 import { Params } from '@angular/router';
 import { NavigatorQueryParamsCleanupService } from '@exense/step-core';
 import { Injectable } from '@angular/core';
-import { TsUtils } from './util/ts-utils';
+import { TS_PARAMS_KEY } from './modules/_common';
 
 @Injectable()
 export class TsNavigatorQueryParamsCleanupService implements NavigatorQueryParamsCleanupService {
   isCleanUpRequired(queryParams: Params): boolean {
-    return !!queryParams?.[TsUtils.TS_PARAMS_KEY];
+    return !!queryParams?.[TS_PARAMS_KEY];
   }
   cleanup(queryParams: Params): Params {
-    const clear = (queryParams?.[TsUtils.TS_PARAMS_KEY] ?? '').split(',');
+    const clear = (queryParams?.[TS_PARAMS_KEY] ?? '').split(',');
     clear.forEach((value: string) => delete queryParams[value]);
-    delete queryParams[TsUtils.TS_PARAMS_KEY];
+    delete queryParams[TS_PARAMS_KEY];
     return queryParams;
   }
 }
