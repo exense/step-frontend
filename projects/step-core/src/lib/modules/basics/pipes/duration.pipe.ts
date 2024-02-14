e41c2eafa246719608db486d0f2ee858a3decc86
dvladir <dragonvladir@gmail.com>
2024-02-14 13:32:37 +0300

    SED-2829 Add a duration column to replace end column

--- projects/step-core/src/lib/modules/basics/pipes/duration.pipe.ts
+++ projects/step-core/src/lib/modules/basics/pipes/duration.pipe.ts
@@ -1,10 +1,31 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DateTime, Duration, DurationUnit } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 
 @Pipe({
   name: 'duration',
 })
 export class DurationPipe implements PipeTransform {
+  transform(end?: number | Date | DateTime, start?: number | Date | DateTime, displayFull = false): string {
+    const endMs = this.getMs(end);
+    const startMs = this.getMs(start);
+    if (!endMs || !startMs) {
+      return '';
+    }
+    const duration = Duration.fromMillis(endMs - startMs).rescale();
+    const day = this.addSuffix(duration.get('days'), 'd');
+    const hour = this.addSuffix(duration.get('hours'), 'h');
+    const minute = this.addSuffix(duration.get('minutes'), 'm');
+    const second = this.addSuffix(duration.get('seconds'), 's');
+    const ms = this.addSuffix(duration.get('milliseconds'), 'ms');
+
+    let partsToDisplay = [day, hour, minute, second, ms].filter((item) => !!item);
+    if (!displayFull) {
+      partsToDisplay = partsToDisplay.slice(0, 2);
+    }
+
+    return partsToDisplay.join(' ');
+  }
+
   private addSuffix(num: number, suffix: string): string {
     if (num <= 0) {
       return '';
@@ -23,20 +44,5 @@ export class DurationPipe implements PipeTransform {
       return value;
     }
     return undefined;
-  }
-
-  transform(end?: number | Date | DateTime, start?: number | Date | DateTime): string {
-    const endMs = this.getMs(end);
-    const startMs = this.getMs(start);
-    if (!endMs || !startMs) {
-      return '';
-    }
-    const duration = Duration.fromMillis(endMs - startMs).rescale();
-    const day = this.addSuffix(duration.get('days'), 'd');
-    const hour = this.addSuffix(duration.get('hours'), 'h');
-    const minute = this.addSuffix(duration.get('minutes'), 'm');
-    const second = this.addSuffix(duration.get('seconds'), 's');
-    const ms = this.addSuffix(duration.get('milliseconds'), 'ms');
-    return [day, hour, minute, second, ms].join(' ').trim();
   }
 }
