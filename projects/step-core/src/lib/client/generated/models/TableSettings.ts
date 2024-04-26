cb11fa6fcd060a0c6ab0a75a68b94966bbb6297d
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2024-04-26 11:26:00 +0200

    SED-2915 fixed invalid generated ColumnSelection property

--- projects/step-core/src/lib/client/generated/models/TableSettings.ts
+++ projects/step-core/src/lib/client/generated/models/TableSettings.ts
@@ -3,13 +3,11 @@
 /* eslint-disable */
 
 import type { ColumnSettings } from './ColumnSettings';
-import type { ColumnSelection } from './ColumnSelection';
 
 export type TableSettings = {
   customFields?: Record<string, any>;
   settingId?: string;
   scope?: Record<string, string>;
   columnSettingList?: Array<ColumnSettings>;
-  columns: Array<ColumnSelection>;
   id?: string;
 };
