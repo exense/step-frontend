a6cba7cfc9317603369b29e61f31932e36223e18
Lucian Baciu <103116491+lucianbaciu@users.noreply.github.com>
2024-05-06 19:36:21 +0300

    SED-3044-fix chart resize (#812)

--- projects/step-frontend/src/lib/modules/timeseries/modules/filter-bar/components/ranger/ts-ranger.component.ts
+++ projects/step-frontend/src/lib/modules/timeseries/modules/filter-bar/components/ranger/ts-ranger.component.ts
@@ -95,7 +95,19 @@ export class TSRangerComponent implements OnInit, AfterViewInit, OnChanges, OnDe
   }
 
   resizeChart() {
+    const chartPadding = 50; // this is the way uplot works
+    const fullWidth = this.uplot.width - chartPadding;
+    const leftSelect = this.uplot.select.left;
+    const width = this.uplot.select.width;
+    const rightSelect = leftSelect + width;
+    const leftPercent = (100 * leftSelect) / fullWidth;
+    const rightPercent = (100 * rightSelect) / fullWidth;
     this.uplot.setSize(this.getSize());
+    let newChartWidth = this.uplot.width - chartPadding;
+
+    const newLeft = (leftPercent / 100) * newChartWidth;
+    const newRight = (rightPercent / 100) * newChartWidth;
+    this.uplot.setSelect({ left: newLeft, width: newRight - newLeft, top: 0, height: this.CHART_HEIGHT }, false);
   }
 
   init(settings: TSRangerSettings) {
