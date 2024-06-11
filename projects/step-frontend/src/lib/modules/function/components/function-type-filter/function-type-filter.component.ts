8486d459d562f1690869dd345ed10e940b65b9f1
Łukasz Lepa <lukasz.lepa@exense.ch>
2024-06-11 11:25:22 +0200

    SED-3174 use ngx-mat-select-search for multiple select

--- projects/step-frontend/src/lib/modules/function/components/function-type-filter/function-type-filter.component.ts
+++ projects/step-frontend/src/lib/modules/function/components/function-type-filter/function-type-filter.component.ts
@@ -1,5 +1,7 @@
-import { Component, forwardRef, inject, TrackByFunction } from '@angular/core';
+import { AfterViewInit, Component, forwardRef, inject, TrackByFunction } from '@angular/core';
 import { ArrayFilterComponent, BaseFilterComponent, FunctionTypeRegistryService, ItemInfo } from '@exense/step-core';
+import { FormControl } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'step-function-type-filter',
@@ -12,7 +14,24 @@ import { ArrayFilterComponent, BaseFilterComponent, FunctionTypeRegistryService,
     },
   ],
 })
-export class FunctionTypeFilterComponent extends ArrayFilterComponent implements BaseFilterComponent<string, unknown> {
+export class FunctionTypeFilterComponent
+  extends ArrayFilterComponent
+  implements AfterViewInit, BaseFilterComponent<string, unknown>
+{
   readonly _functionTypes = inject(FunctionTypeRegistryService).getItemInfos();
   readonly trackByItemInfo: TrackByFunction<ItemInfo> = (index, item) => item.type;
+  filterMultiControl: FormControl<string | null> = new FormControl<string>('');
+  functionTypesFiltered: ItemInfo[] = [...this._functionTypes];
+
+  ngAfterViewInit() {
+    this.filterMultiControl.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe((value) => {
+      if (value) {
+        this.functionTypesFiltered = this._functionTypes.filter((type) =>
+          type.label.toLowerCase().includes(value.toLowerCase()),
+        );
+      } else {
+        this.functionTypesFiltered = [...this._functionTypes];
+      }
+    });
+  }
 }
