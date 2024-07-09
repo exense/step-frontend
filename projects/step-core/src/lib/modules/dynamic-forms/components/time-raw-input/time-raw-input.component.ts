257cfce24f951f11237a84bceebfd042a1e6cd1e
Łukasz Lepa <lukasz.lepa@exense.ch>
2024-07-09 23:12:49 +0200

    SED-3174 add dropdown filter for more components

--- projects/step-core/src/lib/modules/dynamic-forms/components/time-raw-input/time-raw-input.component.ts
+++ projects/step-core/src/lib/modules/dynamic-forms/components/time-raw-input/time-raw-input.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
-import { TimeInputComponent } from '../../../basics/step-basics.module';
-import { NgControl } from '@angular/forms';
+import { Component, DestroyRef, EventEmitter, inject, Input, Output, ViewEncapsulation } from '@angular/core';
+import { TimeInputComponent, TimeUnit } from '../../../basics/step-basics.module';
+import { FormControl, NgControl } from '@angular/forms';
 import { NUMBER_CHARS_POSITIVE_ONLY } from '../../shared/constants';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'step-time-raw-input',
@@ -16,4 +18,22 @@ export class TimeRawInputComponent extends TimeInputComponent {
   @Output() toggleDynamicExpression = new EventEmitter<void>();
 
   readonly allowedChars = NUMBER_CHARS_POSITIVE_ONLY;
+
+  private _destroyRef = inject(DestroyRef);
+
+  filterMultiControl: FormControl<string | null> = new FormControl<string>('');
+  dropdownItemsFiltered: KeyValue<TimeUnit, string>[] = [];
+
+  ngAfterContentInit(): void {
+    this.dropdownItemsFiltered = [...this.measureItems];
+    this.filterMultiControl.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe((value) => {
+      if (value) {
+        this.dropdownItemsFiltered = this.measureItems.filter((item) =>
+          item.value.toLowerCase().includes(value.toLowerCase()),
+        );
+      } else {
+        this.dropdownItemsFiltered = [...this.measureItems];
+      }
+    });
+  }
 }
--- projects/step-frontend/src/lib/os-plugins/modules/script-editor/components/function-type-script/function-type-script.component.ts
+++ projects/step-frontend/src/lib/os-plugins/modules/script-editor/components/function-type-script/function-type-script.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AfterContentInit, Component, DestroyRef, inject } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 import {
   FunctionTypeFormComponent,
   FunctionTypeScriptOption,
   FUNCTION_TYPE_SCRIPT_OPTIONS,
   ScriptLanguage,
+  ItemInfo,
 } from '@exense/step-core';
 import { FunctionScript } from './function-script.interface';
 import {
@@ -13,21 +14,42 @@ import {
   functionTypeScriptFormSetValueToForm,
   functionTypeScriptFormSetValueToModel,
 } from './function-type-script.form';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'step-function-type-script',
   templateUrl: './function-type-script.component.html',
   styleUrls: ['./function-type-script.component.scss'],
 })
-export class FunctionTypeScriptComponent extends FunctionTypeFormComponent<FunctionTypeScriptForm> {
+export class FunctionTypeScriptComponent
+  extends FunctionTypeFormComponent<FunctionTypeScriptForm>
+  implements AfterContentInit
+{
   private _formBuilder = inject(FormBuilder);
+  private _destroyRef = inject(DestroyRef);
 
   protected readonly formGroup = functionTypeScriptFormCreate(this._formBuilder);
   protected readonly functionTypeScriptOptions = inject<FunctionTypeScriptOption[]>(FUNCTION_TYPE_SCRIPT_OPTIONS);
   protected readonly ScriptLanguage = ScriptLanguage;
 
+  filterMultiControl: FormControl<string | null> = new FormControl<string>('');
+  dropdownItemsFiltered: FunctionTypeScriptOption[] = [];
+
   protected get scriptLanguage(): ScriptLanguage | undefined {
     return this.formGroup.controls.scriptLanguage.value;
+  }
+
+  ngAfterContentInit(): void {
+    this.dropdownItemsFiltered = [...this.functionTypeScriptOptions];
+    this.filterMultiControl.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe((value) => {
+      if (value) {
+        this.dropdownItemsFiltered = this.functionTypeScriptOptions.filter((item) =>
+          item.label.toLowerCase().includes(value.toLowerCase()),
+        );
+      } else {
+        this.dropdownItemsFiltered = [...this.functionTypeScriptOptions];
+      }
+    });
   }
 
   override setValueToForm(): void {
