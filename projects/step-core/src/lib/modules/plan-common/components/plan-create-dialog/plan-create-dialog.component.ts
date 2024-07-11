6838f8d20e7d20733b75cf0c4b054c1f562caaf8
Łukasz Lepa <lukasz.lepa@exense.ch>
2024-07-11 11:01:50 +0200

    SED-3174 add filter to plan type

--- projects/step-core/src/lib/modules/plan-common/components/plan-create-dialog/plan-create-dialog.component.ts
+++ projects/step-core/src/lib/modules/plan-common/components/plan-create-dialog/plan-create-dialog.component.ts
@@ -1,23 +1,27 @@
-import { Component, HostListener, inject, TrackByFunction } from '@angular/core';
-import { shareReplay, switchMap, tap } from 'rxjs';
+import { AfterContentInit, Component, DestroyRef, HostListener, inject, TrackByFunction } from '@angular/core';
+import { map, Observable, share, shareReplay, switchMap, tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material/dialog';
 import { CustomFormComponent } from '../../../custom-forms';
 import { StepBasicsModule, DialogRouteResult } from '../../../basics/step-basics.module';
 import { AugmentedPlansService, Plan } from '../../../../client/step-client-module';
 import { ItemInfo, PlanTypeRegistryService } from '../../../custom-registeries/custom-registries.module';
+import { FormControl } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 
 @Component({
   selector: 'step-plan-create-dialog',
   templateUrl: './plan-create-dialog.component.html',
   styleUrls: ['./plan-create-dialog.component.scss'],
   standalone: true,
-  imports: [StepBasicsModule, CustomFormComponent],
+  imports: [StepBasicsModule, CustomFormComponent, NgxMatSelectSearchModule],
 })
-export class PlanCreateDialogComponent {
+export class PlanCreateDialogComponent implements AfterContentInit {
   private _api = inject(AugmentedPlansService);
   private _matDialogRef = inject<MatDialogRef<PlanCreateDialogComponent, DialogRouteResult>>(MatDialogRef);
   private _router = inject(Router);
+  private _destroyRef = inject(DestroyRef);
 
   protected template: string = 'TestCase';
   protected plan: Partial<Plan> = { attributes: {} };
@@ -28,6 +32,35 @@ export class PlanCreateDialogComponent {
   protected planType = this._planTypes.find((planType) => planType.type === 'step.core.plans.Plan')?.type;
 
   readonly artefactTypes$ = this._api.getArtefactTemplates().pipe(shareReplay(1));
+
+  filterMultiControl: FormControl<string | null> = new FormControl<string>('');
+  filterMultiTypeControl: FormControl<string | null> = new FormControl<string>('');
+  dropdownItemsFiltered: ItemInfo[] = [];
+  dropdownItemsTypeFiltered$: Observable<string[]> | undefined;
+
+  ngAfterContentInit(): void {
+    this.dropdownItemsFiltered = [...this._planTypes];
+    this.filterMultiControl.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe((value) => {
+      if (value) {
+        this.dropdownItemsFiltered = this._planTypes.filter((item) =>
+          item.label.toLowerCase().includes(value.toLowerCase()),
+        );
+      } else {
+        this.dropdownItemsFiltered = [...this._planTypes];
+      }
+    });
+
+    this.dropdownItemsTypeFiltered$ = this.artefactTypes$.pipe(share());
+    this.filterMultiTypeControl.valueChanges.pipe(takeUntilDestroyed(this._destroyRef)).subscribe((value) => {
+      if (value) {
+        this.dropdownItemsTypeFiltered$ = this.artefactTypes$.pipe(
+          map((list) => list.filter((item) => item.toLowerCase().includes(value.toLowerCase()))),
+        );
+      } else {
+        this.dropdownItemsTypeFiltered$ = this.artefactTypes$.pipe(share());
+      }
+    });
+  }
 
   save(editAfterSave?: boolean): void {
     this._api
