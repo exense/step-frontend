08d5f07afc2b89b8d04a40a9e9df9fb20c5e8448
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2024-08-22 10:08:36 +0200

    SED-3026 fixed merge conflict

--- projects/step-frontend/src/lib/modules/parameter/parameter.module.ts
+++ projects/step-frontend/src/lib/modules/parameter/parameter.module.ts
@@ -31,7 +31,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [StepCoreModule, StepCommonModule, BrowserAnimationsModule, NgxMatSelectSearchModule],
-  exports: [ParametersListComponent, ParameterSelectionComponent],
+  exports: [ParameterListComponent, ParameterSelectionComponent],
   declarations: [
     ParameterListComponent,
     ParametersKeyComponent,
