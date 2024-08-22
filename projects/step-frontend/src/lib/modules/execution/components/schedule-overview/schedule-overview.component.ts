a8c55d2e4a29f0e8787f998226a10d222a6137e9
Amir Benkoula <amir.benkoula@exense.ch>
2024-08-22 14:15:13 +0200

    SED-3339 use of entity-icon

--- projects/step-frontend/src/lib/modules/execution/components/schedule-overview/schedule-overview.component.ts
+++ projects/step-frontend/src/lib/modules/execution/components/schedule-overview/schedule-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AugmentedSchedulerService, ExecutiontTaskParameters } from '@exense/step-core';
+import { AugmentedSchedulerService, ExecutiontTaskParameters, Plan } from '@exense/step-core';
 
 @Component({
   selector: 'step-schedule-overview',
@@ -12,8 +12,13 @@ export class ScheduleOverviewComponent implements OnInit {
   private taskId?: string;
   private _taskData?: ExecutiontTaskParameters;
 
-  protected error = '';
   protected task = this._taskData;
+
+  protected plan?: Partial<Plan>;
+
+  protected error = '';
+  protected repositoryId?: string;
+  protected repositoryPlanId?: string;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -23,7 +28,10 @@ export class ScheduleOverviewComponent implements OnInit {
       this.taskId = segments[segments.length - 1].path;
       if (this.taskId) {
         this._scheduleApi.getExecutionTaskById(this.taskId).subscribe({
-          next: (task) => (this.task = task),
+          next: (task) => {
+            this.task = task;
+            this.initializeTask();
+          },
           error: () => {
             this.error = 'Invalid Task Id or server error.';
           },
@@ -32,5 +40,36 @@ export class ScheduleOverviewComponent implements OnInit {
         this.error = 'Task Id not found in the URL.';
       }
     });
+  }
+
+  private initializeTask(): void {
+    if (this.task) {
+      if (!this.task.attributes) {
+        this.task.attributes = {};
+      }
+      if (!this.task.executionsParameters) {
+        this.task.executionsParameters = {};
+      }
+      if (!this.task.executionsParameters.customParameters) {
+        this.task.executionsParameters.customParameters = {};
+      }
+
+      const repository = this.task?.executionsParameters?.repositoryObject;
+      if (repository?.repositoryID === 'local') {
+        const planId = repository?.repositoryParameters?.['planid'];
+        if (planId) {
+          const id = planId;
+          const name = this.task.executionsParameters.description ?? '';
+          this.plan = {
+            id,
+            attributes: { name },
+          };
+        }
+      } else {
+        this.repositoryId = repository?.repositoryID;
+        this.repositoryPlanId =
+          repository?.repositoryParameters?.['planid'] ?? repository?.repositoryParameters?.['planId'];
+      }
+    }
   }
 }
