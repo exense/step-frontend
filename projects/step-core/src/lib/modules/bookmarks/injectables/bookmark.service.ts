fcb4997686817d6e7872481d7177abc53ce17c0a
dvladir <dragonvladir@gmail.com>
2024-08-30 12:29:17 +0300

    SED-3353 Left navigation menu collapsing unexpectedly

--- projects/step-core/src/lib/modules/bookmarks/injectables/bookmark.service.ts
+++ projects/step-core/src/lib/modules/bookmarks/injectables/bookmark.service.ts
@@ -2,7 +2,6 @@ import { inject, Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject, map, shareReplay } from 'rxjs';
 import { AugmentedBookmarksService } from '../../../client/step-client-module';
 import { switchMap } from 'rxjs/operators';
-import { MenuEntry } from '../../routing';
 
 @Injectable({
   providedIn: 'root',
@@ -25,26 +24,6 @@ export class BookmarkService implements OnDestroy {
     ),
     map((table) => table.data),
     shareReplay(1),
-  );
-
-  readonly bookmarkMenuItems$ = this.bookmarks$.pipe(
-    map(
-      (bookmarks) =>
-        (bookmarks ?? [])
-          .map((item) => ({
-            title: item.customFields!['label'],
-            id: `${item.customFields!['link']}`,
-            icon: item.customFields!['icon'],
-            position: item.customFields!['position'] || 100,
-            parentId: 'bookmarks-root',
-            weight: 1000 + bookmarks!.length,
-            isBookmark: true,
-            isEnabledFct(): boolean {
-              return true;
-            },
-          }))
-          .sort((a, b) => a.position - b.position) as MenuEntry[],
-    ),
   );
 
   refreshBookmarks(): void {
