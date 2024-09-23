935fc812eeac150d90ba79aa78dc64e3ff34464d
Vladimir <dragonvladir@gmail.com>
2024-09-23 17:14:45 +0300

    SED-3231 Implement notification preset FE (#918)
    * SED-3231 Implement notification preset FE
    
    * SED-2949 Implement Notification Presets. Admin view
    
    * SED-2949 Implement Notification Presets. Improvements + Integration with notification alerts.
    
    * SED-2949 Implement Notification Presets
    
    * SED-2949 Implement Notification Presets
    
    * SED-3231 Improvements

--- projects/step-core/src/lib/modules/basics/components/form-field/form-field.component.ts
+++ projects/step-core/src/lib/modules/basics/components/form-field/form-field.component.ts
@@ -1,19 +1,33 @@
-import { Component, ContentChild, Input, ViewEncapsulation } from '@angular/core';
+import { Component, computed, contentChild, ContentChild, Input, ViewEncapsulation } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { getControlWarningsContainer } from '../../types/form-control-warnings-extension';
 @Component({
   selector: 'step-form-field',
   templateUrl: './form-field.component.html',
   styleUrls: ['./form-field.component.scss'],
   host: {
-    '[class.ng-invalid]': 'control?.invalid',
-    '[class.ng-touched]': 'control?.touched',
+    '[class.ng-invalid]': 'control()?.invalid',
+    '[class.ng-touched]': 'control()?.touched',
+    '[class.step-has-warnings]': 'hasWarnings()',
   },
   encapsulation: ViewEncapsulation.None,
 })
 export class FormFieldComponent {
-  @ContentChild(NgControl)
-  control?: NgControl;
+  /* @ContentChild(NgControl) */
+  control = contentChild(NgControl);
 
-  @Input() alignLabelAddon: 'separate' | 'near' = 'separate';
+  protected hasWarnings = computed(() => {
+    const control = this.control()?.control;
+    if (!control) {
+      return false;
+    }
+    const warningsContainer = getControlWarningsContainer(control);
+    if (!warningsContainer) {
+      return false;
+    }
+    return warningsContainer.hasWarnings();
+  });
+
+  @Input() alignLabelAddon: 'separate' | 'near' | 'fill' = 'separate';
   @Input() showRequiredMarker?: boolean;
 }
