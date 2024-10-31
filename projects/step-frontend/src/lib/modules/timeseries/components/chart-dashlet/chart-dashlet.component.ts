1d507f9ce14b2b6a4ff94535a5c9ff7e01551aff
lucianbaciu <lucian.baciu@exense.ch>
2024-10-31 18:03:37 +0200

    SED-3435-remove missing ttl warning

--- projects/step-frontend/src/lib/modules/timeseries/components/chart-dashlet/chart-dashlet.component.ts
+++ projects/step-frontend/src/lib/modules/timeseries/components/chart-dashlet/chart-dashlet.component.ts
@@ -110,7 +110,6 @@ export class ChartDashletComponent extends ChartDashlet implements OnInit {
   syncGroupSubscription?: Subscription;
   cachedResponse?: TimeSeriesAPIResponse;
   showHigherResolutionWarning = false;
-  showMissingTTLWarning = false;
   collectionResolutionUsed: number = 0;
 
   ngOnInit(): void {
@@ -461,7 +460,6 @@ export class ChartDashletComponent extends ChartDashlet implements OnInit {
     return this._timeSeriesService.getTimeSeries(request).pipe(
       tap((response) => {
         this.showHigherResolutionWarning = response.higherResolutionUsed;
-        this.showMissingTTLWarning = !response.ttlCovered;
         this.collectionResolutionUsed = response.collectionResolution;
         this.cachedResponse = response;
         this.createChart(response);
