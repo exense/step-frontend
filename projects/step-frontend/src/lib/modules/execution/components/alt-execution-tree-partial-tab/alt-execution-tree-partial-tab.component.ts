0f2a44426aacc93b0cd37a83afe8269c2eaa2f5d
dvladir <dragonvladir@gmail.com>
2025-02-18 14:28:58 +0300

    SED-3749 Selection improvements

--- projects/step-frontend/src/lib/modules/execution/components/alt-execution-tree-partial-tab/alt-execution-tree-partial-tab.component.ts
+++ projects/step-frontend/src/lib/modules/execution/components/alt-execution-tree-partial-tab/alt-execution-tree-partial-tab.component.ts
@@ -8,7 +8,7 @@ import {
 import { AggregatedReportViewTreeStateService } from '../../services/aggregated-report-view-tree-state.service';
 import { AltExecutionStateService } from '../../services/alt-execution-state.service';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, filter, map, switchMap } from 'rxjs';
+import { combineLatest, filter, map, shareReplay, switchMap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AltExecutionTabsService, STATIC_TABS } from '../../services/alt-execution-tabs.service';
 
@@ -39,6 +39,12 @@ export class AltExecutionTreePartialTabComponent implements OnInit {
     filter((reportNodeId) => !!reportNodeId),
   );
 
+  private reportNode$ = this.reportNodeId$.pipe(
+    switchMap((nodeId) => this._controllerService.getReportNode(nodeId)),
+    shareReplay(1),
+    takeUntilDestroyed(),
+  );
+
   private range$ = this._executionState.dateRange$.pipe(
     map((range) => {
       if (!range) {
@@ -56,35 +62,33 @@ export class AltExecutionTreePartialTabComponent implements OnInit {
   }
 
   private setupTree(): void {
-    combineLatest([this._executionState.executionId$, this.range$, this.reportNodeId$])
+    combineLatest([this._executionState.executionId$, this.range$, this.reportNode$])
       .pipe(
-        switchMap(([executionId, range, selectedReportNodeId]) => {
-          const request: AggregatedReportViewRequest = { range, selectedReportNodeId };
-          return this._executionsApi.getAggregatedReportView(executionId, request);
+        switchMap(([executionId, range, reportNode]) => {
+          const request: AggregatedReportViewRequest = { range, selectedReportNodeId: reportNode.id };
+          return this._executionsApi
+            .getAggregatedReportView(executionId, request)
+            .pipe(map((tree) => (!tree ? undefined : { tree, artefactId: reportNode.artefactID ?? undefined })));
         }),
         takeUntilDestroyed(this._destroyRef),
       )
-      .subscribe((root) => {
-        if (!root) {
+      .subscribe((result) => {
+        if (!result) {
           return;
         }
-        this._treeState.init(root);
+        const selectedNodeIds = result.artefactId ? [result.artefactId] : [];
+        this._treeState.init(result.tree, { selectedNodeIds });
       });
   }
 
   private initializeTab(): void {
-    this.reportNodeId$
-      .pipe(
-        switchMap((nodeId) => this._controllerService.getReportNode(nodeId)),
-        takeUntilDestroyed(this._destroyRef),
-      )
-      .subscribe((reportNode) => {
-        this._tabsService.addTab(
-          reportNode.id!,
-          `Tree: ${reportNode.name}`,
-          `sub-tree/${reportNode.id!}`,
-          STATIC_TABS.ANALYTICS,
-        );
-      });
+    this.reportNode$.subscribe((reportNode) => {
+      this._tabsService.addTab(
+        reportNode.id!,
+        `Tree: ${reportNode.name}`,
+        `sub-tree/${reportNode.id!}`,
+        STATIC_TABS.ANALYTICS,
+      );
+    });
   }
 }
