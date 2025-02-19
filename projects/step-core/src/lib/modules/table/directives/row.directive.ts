ccd7a9aafb618df570ac4caaeb3c2a741ba3947b
dvladir <dragonvladir@gmail.com>
2025-02-19 18:12:23 +0300

    SED-3747 Improve drag and drop

---
+++ projects/step-core/src/lib/modules/table/directives/row.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, inject, input } from '@angular/core';
+import { RowInfo } from '@exense/step-core';
+
+@Directive({
+  selector: 'tr[stepRow]',
+})
+export class RowDirective {
+  private _elRef = inject<ElementRef<HTMLTableRowElement>>(ElementRef, { self: true });
+  readonly data = input<unknown>(undefined, { alias: 'stepRow' });
+
+  getRowInfo(): RowInfo {
+    const elRef = this._elRef;
+    const data = this.data();
+    return { elRef, data };
+  }
+}
