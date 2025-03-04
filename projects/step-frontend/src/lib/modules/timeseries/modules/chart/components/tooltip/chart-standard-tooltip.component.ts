5591847f58863d9f238f4d509f8c137220855cb7
lucianbaciu <lucian.baciu@exense.ch>
2025-03-05 00:48:58 +0200

    SED-3598-tooltip improvements

--- projects/step-frontend/src/lib/modules/timeseries/modules/chart/components/tooltip/chart-standard-tooltip.component.ts
+++ projects/step-frontend/src/lib/modules/timeseries/modules/chart/components/tooltip/chart-standard-tooltip.component.ts
@@ -4,6 +4,7 @@ import { TooltipContextData } from '../../injectables/tooltip-context-data';
 import { TooltipRowEntry } from '../../types/tooltip-row-entry';
 import { MarkerType } from '@exense/step-core';
 import { SeriesStroke } from '../../../_common/types/time-series/series-stroke';
+import { NgTemplateOutlet } from '@angular/common';
 
 /**
  * This component represents a visual effect for a loading chart.
@@ -13,13 +14,14 @@ import { SeriesStroke } from '../../../_common/types/time-series/series-stroke';
   templateUrl: './chart-standard-tooltip.component.html',
   styleUrls: ['./chart-standard-tooltip.component.scss'],
   standalone: true,
-  imports: [COMMON_IMPORTS],
+  imports: [COMMON_IMPORTS, NgTemplateOutlet],
 })
 export class ChartStandardTooltipComponent {
   data = input.required<TooltipContextData>();
 
   elipsisBefore = false;
   elipsisAfter = false;
+  summaryEntry: TooltipRowEntry | undefined;
 
   timestamp = computed(() => {
     let idx = this.data().idx;
@@ -40,38 +42,35 @@ export class ChartStandardTooltipComponent {
     }
     const hoveredValue: number = contextData.idY!;
     let yPoints: TooltipRowEntry[] = [];
-    let summaryRow: TooltipRowEntry | undefined;
     // first series is x axis (time)
     const chartSeries = contextData.series;
-    console.log(contextData);
     for (let i = 0; i < chartSeries.length; i++) {
       const series = chartSeries[i];
       const bucketValue = series.data[idx];
-      if (series.scale !== TimeSeriesConfig.SECONDARY_AXES_KEY && series.show) {
-        if (bucketValue != undefined) {
-          const executionIds = contextData.parentRef.chartMetadata[i]?.[idx]?.[TimeSeriesConfig.EXECUTION_ID_ATTRIBUTE];
-          let formattedValue = `${Math.trunc(bucketValue)} `;
-          if (settings.yAxisUnit) {
-            formattedValue += settings.yAxisUnit;
+      if (series.show) {
+        if (series.scale !== TimeSeriesConfig.SECONDARY_AXES_KEY) {
+          if (bucketValue != undefined) {
+            const executionIds =
+              contextData.parentRef.chartMetadata[i]?.[idx]?.[TimeSeriesConfig.EXECUTION_ID_ATTRIBUTE];
+            yPoints.push({
+              value: bucketValue,
+              formattedValue: this.formatValue(bucketValue, settings.yAxisUnit),
+              name: series.label || '',
+              // @ts-ignore
+              stroke: series.strokeConfig,
+              executions: executionIds,
+              markerClassName: this.getMarkerClass(series.strokeConfig!),
+            });
           }
-          yPoints.push({
-            value: bucketValue,
-            formattedValue: formattedValue,
-            name: series.label || '',
-            // @ts-ignore
-            stroke: series.strokeConfig,
-            executions: executionIds,
-            markerClassName: this.getMarkerClass(series.strokeConfig!),
-          });
+        } else {
+          if (bucketValue !== null) {
+            this.summaryEntry = {
+              value: bucketValue!,
+              stroke: { color: TimeSeriesConfig.TOTAL_BARS_COLOR, type: MarkerType.SQUARE },
+              name: settings.zAxisLabel || 'Total',
+            };
+          }
         }
-        continue;
-      }
-      if (series.scale === TimeSeriesConfig.SECONDARY_AXES_KEY && bucketValue != null) {
-        summaryRow = {
-          value: bucketValue,
-          stroke: { color: TimeSeriesConfig.TOTAL_BARS_COLOR, type: MarkerType.SQUARE },
-          name: settings.zAxisLabel || 'Total',
-        };
       }
     }
     yPoints.sort((a, b) => (a.value - b.value) * -1);
@@ -99,6 +98,14 @@ export class ChartStandardTooltipComponent {
     this.elipsisAfter = elipsisAfter;
     return yPoints;
   });
+
+  private formatValue(value: number, unit?: string) {
+    let formattedValue = `${Math.trunc(value)} `;
+    if (unit) {
+      formattedValue += unit;
+    }
+    return formattedValue;
+  }
 
   private getMarkerClass(stroke: SeriesStroke): string {
     switch (stroke.type) {
