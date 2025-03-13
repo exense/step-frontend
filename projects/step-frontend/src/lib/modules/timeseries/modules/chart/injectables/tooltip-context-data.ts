172e3f633713441278a230fb6e64d1c47891b5ee
Lucian Baciu <103116491+lucianbaciu@users.noreply.github.com>
2025-03-13 12:15:59 +0200

    SED-3598 links to execution ids for low resolutions (#1055)
    * SED-3598-working on tooltip
    
    * Merge remote-tracking branch 'origin/master' into SED-3658-auto-refresh-on-cross-execution-view
    
    # Conflicts:
    #	projects/step-frontend/src/lib/modules/timeseries/modules/_common/injectables/dashboard-url-params.service.ts
    
    * SED-3598-fixing tooltip
    
    * SED-3598-tooltip improvements
    
    * SED-3598-working on executions menu
    
    * SED-3598-changing tooltip lifecycle
    
    * SED-3598-implement links from metadata
    
    * SED-3598-fetch executions function
    
    * SED-3598-fixing several tooltip issues
    
    * SED-3598-cleanup
    
    * SED-3598-css cleanup
    
    * SED-3598-cleanup
    
    ---------
    
    Co-authored-by: Tim Rasim <neogucky@gmail.com>

--- projects/step-frontend/src/lib/modules/timeseries/modules/chart/injectables/tooltip-context-data.ts
+++ projects/step-frontend/src/lib/modules/timeseries/modules/chart/injectables/tooltip-context-data.ts
@@ -1,10 +1,13 @@
 import * as uPlot from 'uplot';
 import { TSChartSeries } from '../types/ts-chart-series';
+import { TimeSeriesChartComponent } from '../components/time-series-chart/time-series-chart.component';
+import { TooltipParentContainer } from '../types/tooltip-parent-container';
 
 export interface TooltipContextData {
-  idx?: number;
-  idY?: number;
+  idx?: number; // index of X axes
+  idY?: number; // y value
   xValues: number[];
   series: TSChartSeries[];
   chartRef: uPlot;
+  parentRef: TimeSeriesChartComponent;
 }
