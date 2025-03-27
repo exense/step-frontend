27c0a45bcc53817aa6957d5dde1b0e838ea0ef21
dvladir <dragonvladir@gmail.com>
2025-03-27 16:27:43 +0300

    SED-3864 Swich project entity dialog improvement

--- projects/step-core/src/lib/guards/check-entity-guard.factory.ts
+++ projects/step-core/src/lib/guards/check-entity-guard.factory.ts
@@ -1,9 +1,10 @@
-import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
-import { Observable, of, switchMap } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
+import { map, Observable, of, switchMap } from 'rxjs';
 import { inject, Injector, runInInjectionContext } from '@angular/core';
 import { MultipleProjectsService } from '../modules/basics/step-basics.module';
 import { AuthService } from '../modules/auth';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { DEFAULT_PAGE } from '../modules/routing';
 
 type EntityEditLink = Parameters<MultipleProjectsService['confirmEntityEditInASeparateProject']>[1];
 
@@ -22,6 +23,8 @@ export const checkEntityGuardFactory =
     const _multipleProjects = inject(MultipleProjectsService);
     const _snackBar = inject(MatSnackBar);
     const _injector = inject(Injector);
+    const _router = inject(Router);
+    const _defaultPage = inject(DEFAULT_PAGE);
 
     const idParameterName = config.idParameterName ?? 'id';
     const id = config.idExtractor ? config.idExtractor(route) : route.params[idParameterName];
@@ -51,6 +54,12 @@ export const checkEntityGuardFactory =
 
         const entityEditLink = runInInjectionContext(_injector, () => config.getEditorUrl(id, route));
         return _multipleProjects.confirmEntityEditInASeparateProject(entity, entityEditLink, config.entityType);
+      }),
+      map((result) => {
+        if (!result && _router.url === '/') {
+          return _router.parseUrl(_defaultPage());
+        }
+        return result;
       }),
     );
   };
