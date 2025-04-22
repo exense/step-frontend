d57ab96fab37b45a67bea4f092a4b1ef96d6dcdc
Vladimir <dragonvladir@gmail.com>
2025-04-22 16:44:42 +0300

    SED-3934 Plan editor component select all select only visible (#1106)
    Co-authored-by: Tim Rasim <neogucky@gmail.com>

--- projects/step-frontend/src/lib/modules/plan-editor/components/plan-function-list/plan-function-list.component.ts
+++ projects/step-frontend/src/lib/modules/plan-editor/components/plan-function-list/plan-function-list.component.ts
@@ -1,13 +1,16 @@
-import { Component, inject, output, ViewEncapsulation } from '@angular/core';
+import { Component, inject, model, output, ViewEncapsulation } from '@angular/core';
 import {
   AugmentedKeywordsService,
-  tableColumnsConfigProvider,
   AutoDeselectStrategy,
+  BulkSelectionType,
   Keyword,
   selectionCollectionProvider,
   SelectionCollector,
+  TableApiWrapperService,
+  tableColumnsConfigProvider,
+  TableRemoteDataSource,
 } from '@exense/step-core';
-import { map } from 'rxjs';
+import { catchError, filter, map, Observable, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'step-plan-function-list',
@@ -26,23 +29,50 @@ import { map } from 'rxjs';
 })
 export class PlanFunctionListComponent {
   private _selectionCollector = inject<SelectionCollector<string, Keyword>>(SelectionCollector);
-  readonly dataSource = inject(AugmentedKeywordsService).createFilteredTableDataSource();
+  private _tableApi = inject(TableApiWrapperService);
+  readonly dataSource = inject(
+    AugmentedKeywordsService,
+  ).createFilteredTableDataSource() as TableRemoteDataSource<Keyword>;
 
   readonly hasSelection$ = this._selectionCollector.length$.pipe(map((length) => length > 0));
 
-  /** @Output **/
-  addKeywords = output<string[]>();
+  readonly addKeywords = output<string[]>();
 
-  addKeyword(id: string): void {
+  protected selectionType = model<BulkSelectionType>(BulkSelectionType.NONE);
+
+  protected addKeyword(id: string): void {
     this.addKeywords.emit([id]);
   }
 
-  addSelectedKeywords(): void {
-    const ids = [...this._selectionCollector.selected];
-    if (!ids.length) {
-      return;
+  protected addSelectedKeywords(): void {
+    of(this.selectionType())
+      .pipe(
+        switchMap((selectionType) => {
+          const isGetFromRemote =
+            selectionType === BulkSelectionType.ALL || selectionType === BulkSelectionType.FILTERED;
+          return isGetFromRemote
+            ? this.requestKeywordIdsWithoutPagination()
+            : of([...this._selectionCollector.selected]);
+        }),
+        filter((ids) => !!ids.length),
+      )
+      .subscribe((ids) => {
+        this._selectionCollector.clear();
+        this.addKeywords.emit(ids);
+      });
+  }
+
+  private requestKeywordIdsWithoutPagination(): Observable<string[]> {
+    const tableId = this.dataSource.tableId;
+    const request = this.dataSource.getCurrentRequest();
+    if (!request) {
+      return of([]);
     }
-    this._selectionCollector.clear();
-    this.addKeywords.emit(ids);
+    request.skip = 0;
+    request.limit = undefined;
+    return this._tableApi.requestTable<Keyword>(tableId, request).pipe(
+      map((data) => data?.data?.map((item) => item.id!)),
+      catchError(() => of([])),
+    );
   }
 }
