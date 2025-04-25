e66d5a0da45d506cdf0b7d67775df01029541193
dvladir <dragonvladir@gmail.com>
2025-04-25 14:44:20 +0300

    SED-3926 Font size improvements

--- projects/step-frontend/src/lib/modules/execution/services/aggregated-report-view-tree-node-utils.service.ts
+++ projects/step-frontend/src/lib/modules/execution/services/aggregated-report-view-tree-node-utils.service.ts
@@ -63,7 +63,7 @@ export class AggregatedReportViewTreeNodeUtilsService
       isSkipped: false,
       isVisuallySkipped: isParentVisuallySkipped ?? false,
       icon,
-      iconClassName: 'larger-view',
+      iconClassName: 'larger-icon',
       expandable,
       children,
       parentId,
