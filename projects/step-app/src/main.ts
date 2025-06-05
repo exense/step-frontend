64ebadb12f6d52a43e0ef71a5a341e61fca97b10
dvladir <dragonvladir@gmail.com>
2025-06-05 19:23:54 +0300

    SED-3740-remove-module-federation

--- projects/step-app/src/main.ts
+++ projects/step-app/src/main.ts
@@ -1,1 +1,17 @@
-import('./bootstrap').catch((err) => console.error(err));
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { environment } from './environments/environment';
+import { AppModule } from '@exense/step-frontend';
+import { GlobalIndicator } from '@exense/step-core';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+const globalIndicator: GlobalIndicator | undefined = (window as any).globalIndicator;
+globalIndicator?.showMessage?.('Initializing application...');
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
