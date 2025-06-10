ce05deb9bf5ea217b9fd220d96024b4efbf7ec49
dvladir <dragonvladir@gmail.com>
2025-06-10 17:34:39 +0300

    SED-4006 Solve issues with virtual scrolling

--- projects/step-core/src/lib/modules/tree/directives/tree-virtual-scroll.directive.ts
+++ projects/step-core/src/lib/modules/tree/directives/tree-virtual-scroll.directive.ts
@@ -1,9 +1,8 @@
 import { computed, Directive, effect, forwardRef, input } from '@angular/core';
 import { VIRTUAL_SCROLL_STRATEGY } from '@angular/cdk/scrolling';
-import { StepAutosizeVirtualScrollStrategy } from '../types/step-autosize-virtual-scroll-strategy';
+import { StepVirtualScrollStrategy } from '../types/step-virtual-scroll-strategy';
 
-const DEFAULT_BUFFER_PX_MIN = 100;
-const DEFAULT_BUFFER_PX_MAX = 200;
+const MIN_ITEM_SIZE = 47;
 
 @Directive({
   selector: '[stepTreeVirtualScroll]',
@@ -18,30 +17,20 @@ const DEFAULT_BUFFER_PX_MAX = 200;
 })
 export class TreeVirtualScrollDirective {
   readonly containerHeight = input.required<string>({ alias: 'stepTreeVirtualScroll' });
-  readonly minBufferPxInput = input<number | undefined>(undefined, { alias: 'minBufferPx' });
-  readonly maxBufferPxInput = input<number | undefined>(undefined, { alias: 'maxBufferPx' });
+  readonly minItemSizePxInput = input<number | undefined>(undefined, { alias: 'minItemSizePx' });
 
-  private minBufferPx = computed(() => {
-    const minBufferPxInput = this.minBufferPxInput();
-    if (minBufferPxInput !== undefined) {
-      return minBufferPxInput;
+  private minItemSizePx = computed(() => {
+    const minItemSizePxInput = this.minItemSizePxInput();
+    if (minItemSizePxInput !== undefined) {
+      return minItemSizePxInput;
     }
-    return DEFAULT_BUFFER_PX_MIN;
+    return MIN_ITEM_SIZE;
   });
 
-  private maxBufferPx = computed(() => {
-    const maxBufferPxInput = this.maxBufferPxInput();
-    if (maxBufferPxInput !== undefined) {
-      return maxBufferPxInput;
-    }
-    return DEFAULT_BUFFER_PX_MAX;
-  });
+  protected readonly strategy = new StepVirtualScrollStrategy(MIN_ITEM_SIZE);
 
-  readonly strategy = new StepAutosizeVirtualScrollStrategy(DEFAULT_BUFFER_PX_MIN, DEFAULT_BUFFER_PX_MAX);
-
-  private effectUpdateBuffers = effect(() => {
-    const minBufferPx = this.minBufferPx();
-    const maxBufferPx = this.maxBufferPx();
-    this.strategy.updateBufferSize(minBufferPx, maxBufferPx);
+  private effectUpdateMinItemSize = effect(() => {
+    const minItemSizePx = this.minItemSizePx();
+    this.strategy.updateMinItemSize(minItemSizePx);
   });
 }
