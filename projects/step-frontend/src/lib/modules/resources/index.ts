c086a92302120d11f32a7459436cd952c42a5e2b
dvladir <dragonvladir@gmail.com>
2025-06-26 12:17:16 +0300

    Bulk action delete not available on resource page

---
+++ projects/step-frontend/src/lib/modules/resources/index.ts
@@ -0,0 +1,11 @@
+import { ResourceConfigurationDialogComponent } from './components/resource-configuration-dialog/resource-configuration-dialog.component';
+import { ResourceSelectionComponent } from './components/resource-selection/resource-selection.component';
+import { ResourcesListComponent } from './components/resources-list/resources-list.component';
+
+export * from './resources.initializer';
+
+export const RESOURCE_IMPORTS = [
+  ResourceConfigurationDialogComponent,
+  ResourceSelectionComponent,
+  ResourcesListComponent,
+];
