4bbe5f816da95a8e9ddc69cc9e1c6360f45a5b3b
dvladir <dragonvladir@gmail.com>
2025-06-27 16:31:42 +0300

    SED-3740 switch build system

--- projects/step-app/federation.config.js
+++ projects/step-app/federation.config.js
@@ -1,9 +1,8 @@
 const { withNativeFederation, share } = require('@angular-architects/native-federation/config');
-const { config, libraryConfig } = require('./shared-libraries.config');
+const { config } = require('./shared-libraries.config');
 
 module.exports = withNativeFederation({
   shared: share({
     ...config,
-    '@angular/platform-browser': libraryConfig(),
   }),
 });
