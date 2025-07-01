a9f20f658c4558f44da50703cb418fe2c1c88894
lucianbaciu <lucian.baciu@exense.ch>
2025-07-02 01:40:39 +0300

    SED-3999-tooltip fixes

--- projects/step-frontend/src/lib/modules/execution/components/schedule-overview/cross-execution-dashboard/executions-chart-tooltip/executions-chart-tooltip.component.ts
+++ projects/step-frontend/src/lib/modules/execution/components/schedule-overview/cross-execution-dashboard/executions-chart-tooltip/executions-chart-tooltip.component.ts
@@ -13,6 +13,8 @@ import { TooltipContextData } from '../../../../../timeseries/modules/chart/inje
 import { AugmentedTimeSeriesService, ExecutionsService, FetchBucketsRequest } from '@exense/step-core';
 import { TSChartSeries } from '../../../../../timeseries/modules/chart';
 import { of, switchMap } from 'rxjs';
+import { FilterUtils, OQLBuilder } from '../../../../../timeseries/modules/_common';
+import { CrossExecutionDashboardState } from '../cross-execution-dashboard-state';
 
 interface TransformedSeries {
   label: string;
@@ -38,13 +40,11 @@ export class ExecutionsChartTooltipComponent {
   private _changeDetectorRef = inject(ChangeDetectorRef);
   private _executionService = inject(ExecutionsService);
   private _timeSeriesService = inject(AugmentedTimeSeriesService);
+  readonly _state = inject(CrossExecutionDashboardState);
 
   reposition = output<void>();
 
   readonly data = input<TooltipContextData | undefined>(undefined);
-  readonly taskId = input.required<string>();
-
-  readonly EXECUTIONS_LIST_LIMIT = 10;
 
   selectedSeries?: TransformedSeries;
   selectedSeriesExecutions: ExecutionItem[] = [];
@@ -84,14 +84,22 @@ export class ExecutionsChartTooltipComponent {
   fetchExecutionsForSelectedItem(item: TransformedSeries, callback?: () => void) {
     const data = this.data()!;
     const bucketInterval = data.xValues[1] - data.xValues[0];
-    const taskId = this.taskId();
     const limit = 10;
+
+    const statusAttribute = this._state.getViewType() === 'task' ? 'result' : 'rnStatus';
+
+    const oql = new OQLBuilder()
+      .open('and')
+      .append('attributes.metricType = "executions/duration"')
+      .append(FilterUtils.filtersToOQL([this._state.getDashboardFilter()], 'attributes'))
+      // .append(`attributes.result = ${item.label}`)
+      .build();
 
     const request: FetchBucketsRequest = {
       start: item.timestamp,
       end: item.timestamp + bucketInterval,
       numberOfBuckets: 1,
-      oqlFilter: `attributes.metricType = \"executions/duration\" and attributes.taskId = ${taskId} and attributes.result = ${item.label}`,
+      oqlFilter: oql,
       groupDimensions: ['eId'],
       maxNumberOfSeries: 10,
     };
