c96f7d09dc49bf13ac3e721c39f73d2d5a986327
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2025-07-01 23:59:00 +0200

    SED-4057 updated check

--- projects/step-frontend/src/lib/modules/errors-view/pipes/unique-errors.pipe.ts
+++ projects/step-frontend/src/lib/modules/errors-view/pipes/unique-errors.pipe.ts
@@ -10,10 +10,10 @@ export class UniqueErrorsPipe implements PipeTransform {
   transform(errors: ErrorViewItem[]): ErrorViewItem[] {
     const seen = new Set<string>();
     return errors.filter((error) => {
-      if (seen.has(error.id)) {
+      if (seen.has(error.message)) {
         return false;
       }
-      seen.add(error.id);
+      seen.add(error.message);
       return true;
     });
   }
