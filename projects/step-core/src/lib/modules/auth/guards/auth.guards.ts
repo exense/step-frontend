fabe8cec419955745ed3c81f6ab4b025ef11b4c7
Vladimir <dragonvladir@gmail.com>
2025-07-18 15:45:55 +0300

    SED-4086 Login overwrites current tenant (#1175)
    Co-authored-by: Tim Rasim <neogucky@gmail.com>

--- projects/step-core/src/lib/modules/auth/guards/auth.guards.ts
+++ projects/step-core/src/lib/modules/auth/guards/auth.guards.ts
@@ -1,9 +1,9 @@
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../injectables/auth.service';
 import { NavigatorService } from '../../routing';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (_route: ActivatedRouteSnapshot) => {
   const _authService = inject(AuthService);
   const _navigator = inject(NavigatorService);
 
@@ -11,7 +11,7 @@ export const authGuard: CanActivateFn = () => {
     return true;
   }
 
-  _navigator.navigateLogin();
+  _navigator.navigateLogin(_route.queryParams);
   return false;
 };
 
