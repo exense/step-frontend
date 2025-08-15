336d88a68ca1b04906288d7e1fccd6499253ff77
dvladir <dragonvladir@gmail.com>
2025-08-15 12:27:37 +0300

    SED-4179 Remove 'select all filtered' for TC tables bulkselection

--- projects/step-core/src/lib/modules/entities-selection/components/bulk-selection-label/bulk-selection-label.component.ts
+++ projects/step-core/src/lib/modules/entities-selection/components/bulk-selection-label/bulk-selection-label.component.ts
@@ -1,8 +1,9 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, inject, input, Input, Output } from '@angular/core';
 import { of } from 'rxjs';
 import { SelectionCollector } from '../../services/selection-collector/selection-collector';
 import { BulkSelectionType } from '../../shared/bulk-selection-type.enum';
 import { HasFilter } from '../../services/has-filter';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'step-bulk-selection-label',
@@ -10,13 +11,22 @@ import { HasFilter } from '../../services/has-filter';
   styleUrls: ['./bulk-selection-label.component.scss'],
 })
 export class BulkSelectionLabelComponent<KEY, ENTITY> {
-  readonly hasFilter$ = inject(HasFilter, { optional: true })?.hasFilter$ ?? of(false);
+  private hasFilter$ = inject(HasFilter, { optional: true })?.hasFilter$ ?? of(false);
 
   readonly BulkSelectionType = BulkSelectionType;
   @Input() selectionCollector?: SelectionCollector<KEY, ENTITY>;
   @Input() selectionType?: BulkSelectionType;
   @Output() selectionTypeChange = new EventEmitter<BulkSelectionType>();
   @Input() isDisabled = false;
+
+  private hasFilter = toSignal(this.hasFilter$, { initialValue: false });
+  readonly dontUseFilterLabel = input(false);
+
+  protected readonly useFilterLabel = computed(() => {
+    const hasFilter = this.hasFilter();
+    const dontUseFilterLabel = this.dontUseFilterLabel();
+    return hasFilter && !dontUseFilterLabel;
+  });
 
   clearSelection(): void {
     if (this.isDisabled) {
