c84a0743c48bbf575ebbd383d0a906145d183fcb
dvladir <dragonvladir@gmail.com>
2025-10-21 14:16:16 +0300

    SED-4264 FE - use new dedicated endpoint for node-measurements

--- projects/step-frontend/src/lib/modules/artefacts/component/call-keyword-report-details/call-keyword-report-details.component.ts
+++ projects/step-frontend/src/lib/modules/artefacts/component/call-keyword-report-details/call-keyword-report-details.component.ts
@@ -6,7 +6,6 @@ import {
   Measure,
   ReportNode,
   TableDataSource,
-  TableLocalDataSource,
   TableRemoteDataSourceFactoryService,
 } from '@exense/step-core';
 import { KeywordReportNode } from '../../types/keyword.report-node';
@@ -89,19 +88,6 @@ export class CallKeywordReportDetailsComponent extends BaseReportDetailsComponen
   protected readonly failedChildren = toSignal(this.failedChildren$, { initialValue: [] });
 
   protected readonly measuresDataSource = computed(() => {
-    const measures = this.node()?.measures;
-    if (!measures?.length) {
-      return undefined;
-    }
-    return new TableLocalDataSource(
-      measures,
-      TableLocalDataSource.configBuilder<Measure>()
-        .addSortNumberPredicate('duration', (item: Measure) => item.duration)
-        .build(),
-    );
-  });
-
-  protected readonly measuresDataSource2 = computed(() => {
     const id = this.node()?.id;
     if (!id) {
       return undefined;
@@ -141,7 +127,7 @@ export class CallKeywordReportDetailsComponent extends BaseReportDetailsComponen
 
   private createMeasurementsDataSource(reportNodeId: string): TableDataSource<Measure> {
     return this._dataSourceFactory.createDataSource(
-      'measurements',
+      'reportMeasurements',
       {
         begin: 'begin',
         name: 'name',
