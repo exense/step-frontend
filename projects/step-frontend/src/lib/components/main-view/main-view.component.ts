dca8e12a4c4bb7c1c4aefc0f862b2a525b8a2c15
dvladir <dragonvladir@gmail.com>
2025-10-29 19:34:51 +0300

    SED-4286 Scroll to top after navigation ends

--- projects/step-frontend/src/lib/components/main-view/main-view.component.ts
+++ projects/step-frontend/src/lib/components/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, TrackByFunction } from '@angular/core';
+import { Component, inject, TrackByFunction, viewChild } from '@angular/core';
 import {
   AppConfigContainerService,
   AuthService,
@@ -6,6 +6,10 @@ import {
   ViewRegistryService,
 } from '@exense/step-core';
 import { MatDialog } from '@angular/material/dialog';
+import { CdkScrollable } from '@angular/cdk/overlay';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'step-main-view',
@@ -16,11 +20,21 @@ import { MatDialog } from '@angular/material/dialog';
 export class MainViewComponent {
   private _viewRegistry = inject(ViewRegistryService);
   private _matDialog = inject(MatDialog);
+  private _router = inject(Router);
   readonly _appConfig = inject(AppConfigContainerService);
   readonly _authService = inject(AuthService);
 
   readonly navBarRightMenuItems = this._viewRegistry.getDashlets('menu/navbar/right');
   readonly adminAlerts = this._viewRegistry.getDashlets('admin/alerts');
+
+  private mainScrollableContent = viewChild('mainScrollableContent', { read: CdkScrollable });
+
+  private navigationEndSubscription = this._router.events
+    .pipe(
+      filter((event) => event instanceof NavigationEnd),
+      takeUntilDestroyed(),
+    )
+    .subscribe(() => this.mainScrollableContent()?.scrollTo?.({ top: 0, left: 0 }));
 
   addBookmark(): void {
     this._matDialog.open(BookmarkCreateDialogComponent);
