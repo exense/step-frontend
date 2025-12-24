a0cae89b6cab2f09a79ec659a9adb7b23e3e9eaa
lucian-baciu <lucian.baciu@wedosoft.co>
2025-12-25 01:19:11 +0200

    SED-4450-analyics improvements

--- projects/step-frontend/src/lib/modules/timeseries/modules/filter-bar/components/perfomance-view-time-selection/performance-view-time-selection.component.ts
+++ projects/step-frontend/src/lib/modules/timeseries/modules/filter-bar/components/perfomance-view-time-selection/performance-view-time-selection.component.ts
@@ -3,9 +3,11 @@ import {
   DestroyRef,
   EventEmitter,
   inject,
+  input,
   Input,
   OnInit,
   Output,
+  signal,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
@@ -22,6 +24,7 @@ import { TSRangerSettings } from '../ranger/ts-ranger-settings';
 import { TSRangerComponent } from '../ranger/ts-ranger.component';
 import { FindBucketsRequestBuilder } from '../../types/find-buckets-request-builder';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { sign } from 'chart.js/helpers';
 
 @Component({
   selector: 'step-execution-time-selection',
@@ -33,6 +36,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 })
 export class PerformanceViewTimeSelectionComponent implements OnInit {
   @Input() context!: TimeSeriesContext;
+  showSpinnerWhileLoading = input<boolean>(false);
 
   @Output() rangerLoaded = new EventEmitter<void>();
 
@@ -43,6 +47,8 @@ export class PerformanceViewTimeSelectionComponent implements OnInit {
 
   private _timeSeriesService = inject(TimeSeriesService);
   private _destroyRef = inject(DestroyRef);
+
+  isLoading = signal<boolean>(false);
 
   ngOnInit(): void {
     if (!this.context) {
@@ -73,6 +79,7 @@ export class PerformanceViewTimeSelectionComponent implements OnInit {
   }
 
   createRanger(context: TimeSeriesContext): Observable<TimeSeriesAPIResponse> {
+    this.isLoading.set(true);
     const customFiltering = JSON.parse(JSON.stringify(this.context.getFilteringSettings())) as TsFilteringSettings;
     customFiltering.filterItems = []; // ignore visible filters.
     const request = new FindBucketsRequestBuilder()
@@ -105,6 +112,7 @@ export class PerformanceViewTimeSelectionComponent implements OnInit {
             },
           ],
         };
+        this.isLoading.set(false);
       }),
     );
   }
