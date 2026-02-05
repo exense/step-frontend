affd0a75a14c23bffde013d14421881d679f4534
Tim Rasim <neogucky@gmail.com>
2026-02-05 12:37:55 +0100

    SED-4488  poor performance of the steps table when filtering by statuses (#1318)
    * SED-4488 Poor performance of the steps table when filtering by statuses
    
    * SED-4488 Poor performance of the steps table when filtering by statuses
    
    * SED-4500 fix force loading for tables
    
    * SED-4488 support inFilterCondition for TC list
    
    ---------
    
    Co-authored-by: David Stephan <david.stephan@exense.ch>
    Co-authored-by: dvladir <dragonvladir@gmail.com>
    Co-authored-by: Tim Rasim <2033832+neogucky@users.noreply.github.com>

--- projects/step-core/src/lib/modules/basics/step-basics.module.ts
+++ projects/step-core/src/lib/modules/basics/step-basics.module.ts
@@ -405,3 +405,4 @@ export * from './utils/resource-id';
 export * from './pipes/number-separate-thousands.pipe';
 export * from './utils/smart-switch-map';
 export * from './utils/duration-switch-map';
+export * from './utils/combine-latest-with-track-changes';
