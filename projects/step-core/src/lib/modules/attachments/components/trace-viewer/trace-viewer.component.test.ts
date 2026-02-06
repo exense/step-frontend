e7e401f4571d2bcab807e08bf9425e18b6fce94b
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2026-02-06 13:24:19 +0100

    SED-4412 solved E2E execution issue

--- projects/step-core/src/lib/modules/attachments/components/trace-viewer/trace-viewer.component.test.ts
+++ projects/step-core/src/lib/modules/attachments/components/trace-viewer/trace-viewer.component.test.ts
@@ -1,13 +1,11 @@
 import { TraceViewerComponent } from './trace-viewer.component';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { APP_HOST } from '../../../../client/_common';
-import { By } from '@angular/platform-browser';
 import { ComponentRef } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 describe('Trace Viewer', () => {
   let componentRef: ComponentRef<TraceViewerComponent>;
-  let fixture: ComponentFixture<TraceViewerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,28 +18,22 @@ describe('Trace Viewer', () => {
       ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TraceViewerComponent);
-    componentRef = fixture.componentRef;
-    fixture.detectChanges();
+    componentRef = TestBed.createComponent(TraceViewerComponent).componentRef;
   });
 
   it('Initial view', async () => {
-    const iframe = fixture.debugElement.query(By.css('iframe'));
-    expect(iframe.nativeElement.src).toEqual('https://step.ch/trace-viewer/');
+    const url = (componentRef.instance as any).traceViewerUrl();
+    expect(url).toEqual('https://step.ch/trace-viewer/');
   });
 
   it('Set report url', async () => {
     componentRef.setInput('reportUrl', 'https://step.ch/reports/1');
-    fixture.detectChanges();
-    await fixture.whenStable();
-    const iframe = fixture.debugElement.query(By.css('iframe'));
-    expect(iframe.nativeElement.src).toEqual('https://step.ch/trace-viewer/?trace=https://step.ch/reports/1');
+    const url = (componentRef.instance as any).traceViewerUrl();
+    expect(url).toEqual('https://step.ch/trace-viewer/?trace=https://step.ch/reports/1');
   });
 
   it('Oper url', async () => {
     componentRef.setInput('reportUrl', 'https://step.ch/reports/1');
-    fixture.detectChanges();
-    await fixture.whenStable();
 
     const doc = TestBed.inject(DOCUMENT);
     const spyWindowOpen = jest.spyOn(doc.defaultView!, 'open').mockImplementation(() => {});
