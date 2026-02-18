c6aba3604cae5d2282923733489069e8afa585a8
Tim Rasim <2033832+neogucky@users.noreply.github.com>
2026-02-18 11:29:44 +0100

    SED-4453 refactored code to use component for non-count pagination

---
+++ projects/step-frontend/src/lib/modules/execution/components/table-counts-toggle/table-counts-toggle.component.ts
@@ -0,0 +1,30 @@
+import { Component, computed, input, output } from '@angular/core';
+
+@Component({
+  selector: 'step-table-counts-toggle',
+  template: `
+    <button
+      type="button"
+      mat-icon-button
+      [color]="enabled() ? 'primary' : undefined"
+      [matTooltip]="tooltipText()"
+      (click)="toggle.emit()"
+    >
+      <step-icon name="loader" />
+    </button>
+  `,
+  standalone: false,
+})
+export class TableCountsToggleComponent {
+  readonly enabled = input(false);
+  readonly itemsLabel = input('items');
+  readonly toggle = output<void>();
+
+  protected readonly tooltipText = computed(() => {
+    const itemsLabel = this.itemsLabel();
+    if (this.enabled()) {
+      return `Click to disable loading the total number of ${itemsLabel} to enhance performance.`;
+    }
+    return `Click to load the total number of ${itemsLabel}. Note: this may slow down performance for large numbers of ${itemsLabel}`;
+  });
+}
