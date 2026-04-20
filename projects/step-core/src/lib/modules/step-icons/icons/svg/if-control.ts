1384f15bd94cedb3810b50d5435f2012074df97c
Tim Rasim <neogucky@gmail.com>
2026-04-20 09:10:06 +0200

    SED-3986 Improve icons for controls (#1350)
    * SED-3986 Added new entity icons
    
    * SED-3986 updated icons to support bg fill
    
    * SED-3986 fixed stroke color of assert metric
    
    * SED-3986 added sleep icon and solved icon rendering issue
    
    * SED-3986 improved icon generation scripts output
    
    * SED-3986 improved icon naming
    
    * SED-3986 added name prefix for svg classes
    
    * improved icon class
    
    ---------
    
    Co-authored-by: Tim Rasim <2033832+neogucky@users.noreply.github.com>

---
+++ projects/step-core/src/lib/modules/step-icons/icons/svg/if-control.ts
@@ -0,0 +1,15 @@
+export const IfControl = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="feather feather-if-control">
+    
+  <defs>
+    <style>
+      .if-control-st0 {
+        fill: none;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+        stroke-width: 2px;
+      }
+    </style>
+  </defs>
+  <rect stroke="currentColor" class="if-control-st0" x="5.81" y="5.81" width="12.37" height="12.37" transform="translate(-4.97 12) rotate(-45)"/>
+
+</svg>`;
