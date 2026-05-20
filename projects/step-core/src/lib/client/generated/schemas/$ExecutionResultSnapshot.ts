0ae0eb17cb821e700c1e802623a768036dfbc492
lucian-baciu <lucian.baciu@wedosoft.co>
2026-05-20 03:32:14 +0300

    SED-4166-added missing generated files

---
+++ projects/step-core/src/lib/client/generated/schemas/$ExecutionResultSnapshot.ts
@@ -0,0 +1,20 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export const $ExecutionResultSnapshot = {
+  properties: {
+    id: {
+      type: 'string',
+    },
+    status: {
+      type: 'Enum',
+    },
+    result: {
+      type: 'Enum',
+    },
+    startTime: {
+      type: 'number',
+      format: 'int64',
+    },
+  },
+} as const;
