0b6cfe0f5a27dbe7a351e8459c3c570dcabb1656
Tim Rasim <neogucky@gmail.com>
2026-06-11 14:13:13 +0200

    SED-4751 Issues with drilldown view II (#1395)
    * SED-4751 solved tree scrolling issue
    
    * SED-4751 fixed node status update logic
    
    * SED-4751 fixed icon gap for drilldown status
    
    * SED-4751 solved issue with drilldown pagination
    
    * SED-4751 fixed accidental change to execution list icons
    
    * SED-4751 solved execution tree content icon layout issue
    
    * SED-4751 fixed paddings
    
    * SED-4751 improved tree icon alignment
    
    * SED-4751 adjusted icon spacing
    
    * SED-4751 fixed review comment
    
    ---------
    
    Co-authored-by: Tim Rasim <2033832+neogucky@users.noreply.github.com>

---
+++ projects/step-frontend/src/lib/modules/execution/shared/alt-report-node-keywords-list-config.tokens.ts
@@ -0,0 +1,6 @@
+import { InjectionToken } from '@angular/core';
+
+export const ALT_REPORT_NODE_KEYWORDS_TABLE_ID = new InjectionToken<string>('Alt report node keywords table id');
+export const ALT_REPORT_NODE_LIST_DEFAULT_PAGE_SIZE = new InjectionToken<number>(
+  'Alt report node list default page size',
+);
