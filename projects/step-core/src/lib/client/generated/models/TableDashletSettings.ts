8ee18f26e593e5559a7bec74533cb76a11cd9a79
Jérôme Comte <jerome.comte@exense.ch>
2026-08-06 17:00:40 +0200

    SED-4848 Make time-series aggregations configurable on both axes (#1429)
    * SED-4848 Make aggregation of charts configurable on both axes
    
    * SED-4848 Make aggregation of charts configurable on both axes
    
    * SED-4848 Make aggregation of charts configurable on both axes
    
    * SED-4848 Make aggregation of tables configurable on both axes
    
    * SED-4848 Rollback behaviour of the secondary axis and rename it total axis
    
    * SED-4848 Renaming aggregation mode switch to single - two-stage + minor design changes
    
    * SED-4848 Final review of labels and tooltips
    
    * SED-4848 Renaming aggregation modes in code
    
    * SED-4848 PR feedback
    
    * SED-4848 Refactoring configuration of metric aggregation
    
    * SED-4848 review comments
    
    * SED-4848 added unit tests
    
    * SED-4848 implementing sampled average aggregation
    
    ---------
    
    Co-authored-by: Tim Rasim <2033832+neogucky@users.noreply.github.com>
    Co-authored-by: David Stephan <david.stephan@exense.ch>

--- projects/step-core/src/lib/client/generated/models/TableDashletSettings.ts
+++ projects/step-core/src/lib/client/generated/models/TableDashletSettings.ts
@@ -3,7 +3,9 @@
 /* eslint-disable */
 
 import type { ColumnSelection } from './ColumnSelection';
+import type { MetricAggregation } from './MetricAggregation';
 
 export type TableDashletSettings = {
   columns: Array<ColumnSelection>;
+  aggregation?: MetricAggregation;
 };
