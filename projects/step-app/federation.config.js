cdc8a14fccd8accf8f6107417244fd5386cecf56
dvladir <dragonvladir@gmail.com>
2026-08-07 16:51:32 +0300

    SED-3470 Fixes

--- projects/step-app/federation.config.js
+++ projects/step-app/federation.config.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const { withNativeFederation, share } = require('@angular-architects/native-federation/config');
 const { config, skip } = require('./shared-libraries.config');
 
 module.exports = withNativeFederation({
   name: '@exense/step-app',
   skip,
-  shared: share({
-    ...config,
-  }),
+  shared: share(
+    {
+      ...config,
+    },
+    path.resolve(__dirname, '../..'),
+  ),
 });
